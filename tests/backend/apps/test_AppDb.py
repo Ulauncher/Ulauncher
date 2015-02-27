4bd486365a37fe09d70a02230fc876c2cb5cd904
Aleksandr Gornostal <sanya.gornostal@gmail.com>
2015-02-27 22:54:32 +0200

    rewritten unit tests using pytest

--- tests/backend/apps/test_AppDb.py
+++ tests/backend/apps/test_AppDb.py
@@ -1,82 +1,59 @@
-#!/usr/bin/python
-# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-### BEGIN LICENSE
-# This file is in the public domain
-### END LICENSE
-
-import sys
 import os
-import unittest
-from tempfile import gettempdir
-
+import pytest
+import mock
 from ulauncher.backend.apps.AppDb import AppDb
 
 
-class TestAppDb(unittest.TestCase):
-    """AppDb"""
+class TestAppDb(object):
 
-    def get_temp_db_name(self):
-        db_file = os.path.join(gettempdir(), 'testdb')
-        try:
-            os.unlink(db_file)
-        except:
-            pass
-        return db_file
+    @pytest.fixture
+    def db_name(self, tmpdir):
+        return os.path.join(str(tmpdir), 'testdb')
 
-    def get_app_db(self):
-        try:
-            return AppDb(self.get_temp_db_name()).open()
-        except Exception as e:
-            self.fail("Exception '%s' was raised unexpectedly: %s" % (type(e), e))
+    @pytest.fixture
+    def app_db(self, db_name):
+        return AppDb(db_name).open()
+
+    @pytest.fixture
+    def db_with_data(self, app_db):
+        app_db.put({'name': 'john', 'description': 'test', 'desktop_file': 'john.desktop', 'icon': 'icon'})
+        app_db.put({'name': 'james', 'description': 'test', 'desktop_file': 'james.desktop', 'icon': 'icon'})
+        app_db.put({'name': 'o.jody', 'description': 'test', 'desktop_file': 'o.jdy.desktop', 'icon': 'icon'})
+        app_db.put({'name': 'sandy', 'description': 'test', 'desktop_file': 'sandy.desktop', 'icon': 'icon'})
+        app_db.put({'name': 'sane', 'description': 'test', 'desktop_file': 'jane.desktop', 'icon': 'icon'})
+        return app_db
 
     def test_open_raises_ioerror(self):
         """It raises IOError if 'name' is a directory"""
-        with self.assertRaises(IOError):
+
+        with pytest.raises(IOError):
             AppDb('/tmp').open()
 
-    def test_open_doesnt_raise(self):
-        """It doesn't raise errors"""
-
-        self.get_app_db()
-
-    def test_commit(self):
+    def test_commit(self, db_name):
         """It saves changes to disk"""
 
-        file_name = os.path.join(gettempdir(), 'test_commit')
-        db = AppDb(file_name).open()
+        db = AppDb(db_name).open()
         db.put({"desktop_file": "test_commit", "name": "hello"})
         db.commit()
 
-        db = AppDb(file_name).open()
-        self.assertEqual(db.find('hello')[0]['desktop_file'], 'test_commit')
+        db = AppDb(db_name).open()
+        db.find('hello')[0]['desktop_file'] == 'test_commit'
 
-    def get_db_with_data(self):
-        db = self.get_app_db()
-        db.put({'name': 'john', 'description': 'test', 'desktop_file': 'john.desktop', 'icon': 'icon'})
-        db.put({'name': 'james', 'description': 'test', 'desktop_file': 'james.desktop', 'icon': 'icon'})
-        db.put({'name': 'o.jody', 'description': 'test', 'desktop_file': 'o.jdy.desktop', 'icon': 'icon'})
-        db.put({'name': 'sandy', 'description': 'test', 'desktop_file': 'sandy.desktop', 'icon': 'icon'})
-        db.put({'name': 'sane', 'description': 'test', 'desktop_file': 'jane.desktop', 'icon': 'icon'})
-        return db
-
-    def test_find_max_results(self):
+    def test_find_max_results(self, db_with_data):
         """It returns no more than limit"""
 
-        db = self.get_db_with_data()
-        self.assertEqual(len(db.find('j', limit=3)), 3)
-        self.assertEqual(len(db.find('j', limit=2)), 2)
+        assert len(db_with_data.find('j', limit=3)) == 3
+        assert len(db_with_data.find('j', limit=2)) == 2
 
-    def test_find_filters_by_min_score(self):
+    def test_find_filters_by_min_score(self, db_with_data):
         """It returns matches only if score > min_score"""
 
-        db = self.get_db_with_data()
-        self.assertEqual(len(db.find('jo', min_score=50)), 2)
-        self.assertEqual(len(db.find('jo', min_score=92)), 0)
+        len(db_with_data.find('jo', min_score=50)) == 2
+        len(db_with_data.find('jo', min_score=92)) == 0
 
-    def test_remove(self):
-        db = self.get_app_db()
-        db.put({'name': 'john', 'description': 'test', 'desktop_file': 'john.desktop', 'icon': 'icon'})
-        db.put({'name': 'james', 'description': 'test', 'desktop_file': 'james.desktop', 'icon': 'icon'})
-        self.assertTrue(db.records.get('james'))
-        db.remove('james')
-        self.assertFalse(db.records.get('james'))
+    def test_remove(self, app_db):
+        app_db.put({'name': 'john', 'description': 'test', 'desktop_file': 'john.desktop', 'icon': 'icon'})
+        app_db.put({'name': 'james', 'description': 'test', 'desktop_file': 'james.desktop', 'icon': 'icon'})
+        assert app_db.records.get('james')
+        app_db.remove('james')
+        assert not app_db.records.get('james')
--- tests/results/test_ResultItem.py
+++ tests/results/test_ResultItem.py
@@ -1,94 +1,101 @@
 # -*- Mode: Python; coding: utf-8;
 
-import unittest
+import pytest
 import mock
 from gi.repository import Gtk, GdkPixbuf
 from ulauncher.results.ResultItem import ResultItem
 
 
-class TestResultItem(unittest.TestCase):
+class TestResultItem(object):
 
-    def setUp(self):
-        self.result_item = ResultItem()
+    @pytest.fixture
+    def result_item(self, builder):
+        result_item = ResultItem()
+        result_item.set_builder(builder)
+        return result_item
 
-        self.builderMock = mock.MagicMock()
-        self.builderMock.get_object.return_value = mock.MagicMock()
-        self.result_item.set_builder(self.builderMock)
+    @pytest.fixture
+    def builder(self):
+        # builder.get_object.return_value = mock.MagicMock()
+        return mock.MagicMock()
 
-    @mock.patch.object(ResultItem, 'set_shortcut', autospec=True)
-    def test_set_index(self, mock_set_shortcut):
-        self.result_item.set_index(2)
-        mock_set_shortcut.assert_called_once_with(self.result_item, 'Alt+3')
+    @pytest.fixture
+    def pixbuf(self):
+        return mock.Mock(spec=GdkPixbuf.Pixbuf)
 
-    @mock.patch('gi.repository.Gtk.StyleContext.add_class')
-    @mock.patch.object(ResultItem, 'set_shortcut', autospec=True)
-    def test_select(self, mock_set_shortcut, mock_add_class):
-        self.result_item.select()
-        mock_set_shortcut.assert_called_once_with(self.result_item, '⏎')
-        mock_add_class.assert_called_once_with('selected')
-
-    @mock.patch('gi.repository.Gtk.StyleContext.remove_class')
-    @mock.patch.object(ResultItem, 'set_shortcut', autospec=True)
-    def test_deselect(self, mock_set_shortcut, mock_remove_class):
-        self.result_item.deselect()
-        mock_set_shortcut.assert_called_once_with(self.result_item, self.result_item.shortcut)
-        mock_remove_class.assert_called_once_with('selected')
-
-    @mock.patch.object(ResultItem, 'load_icon')
-    @mock.patch.object(ResultItem, 'set_default_icon')
-    def test_set_icon_called_with_path(self, mock_set_default_icon, mock_load_icon):
-        pixbuf = mock.Mock(spec=GdkPixbuf.Pixbuf)
+    @pytest.fixture
+    def mock_load_icon(self, pixbuf, result_item, mocker):
+        mock_load_icon = mocker.patch.object(result_item, 'load_icon')
         mock_load_icon.return_value = pixbuf
+        return mock_load_icon
 
+    def test_set_index(self, result_item, mocker):
+        mock_set_shortcut = mocker.patch.object(result_item, 'set_shortcut')
+
+        result_item.set_index(2)
+        mock_set_shortcut.assert_called_once_with('Alt+3')
+
+    def test_select(self, result_item, mocker):
+        mock_set_shortcut = mocker.patch.object(result_item, 'set_shortcut')
+        mock_get_style_context = mocker.patch.object(result_item, 'get_style_context')
+
+        result_item.select()
+        mock_set_shortcut.assert_called_once_with('⏎')
+        mock_get_style_context.return_value.add_class.assert_called_once_with('selected')
+
+    def test_deselect(self, result_item, mocker):
+        mock_set_shortcut = mocker.patch.object(result_item, 'set_shortcut')
+        mock_get_style_context = mocker.patch.object(result_item, 'get_style_context')
+
+        result_item.deselect()
+        mock_set_shortcut.assert_called_once_with(result_item.shortcut)
+        mock_get_style_context.return_value.remove_class.assert_called_once_with('selected')
+
+    def test_set_icon_called_with_path(self, result_item, builder, mock_load_icon, pixbuf):
         iconWgt = mock.MagicMock()
-        self.builderMock.get_object.return_value = iconWgt
+        builder.get_object.return_value = iconWgt
 
-        self.result_item.set_icon('path')
-        self.builderMock.get_object.assert_called_with('item-icon')
+        result_item.set_icon('path')
+        builder.get_object.assert_called_with('item-icon')
         mock_load_icon.assert_called_once_with('path')
         iconWgt.set_from_pixbuf.assert_called_once_with(pixbuf)
 
-    @mock.patch.object(ResultItem, 'load_icon')
-    @mock.patch.object(ResultItem, 'set_default_icon')
-    def test_set_icon_called_with_pixbuf(self, mock_set_default_icon, mock_load_icon):
-        pixbuf = mock.Mock(spec=GdkPixbuf.Pixbuf)
-        mock_load_icon.return_value = pixbuf
-
+    def test_set_icon_called_with_pixbuf(self, result_item, builder, mock_load_icon, pixbuf):
         iconWgt = mock.MagicMock()
-        self.builderMock.get_object.return_value = iconWgt
+        builder.get_object.return_value = iconWgt
 
-        self.result_item.set_icon(pixbuf)
-        self.builderMock.get_object.assert_called_with('item-icon')
+        result_item.set_icon(pixbuf)
+        builder.get_object.assert_called_with('item-icon')
         assert not mock_load_icon.called
         iconWgt.set_from_pixbuf.assert_called_once_with(pixbuf)
 
-    def test_set_name(self):
-        self.result_item.set_name('test name')
-        self.builderMock.get_object.return_value.set_text.assert_called_with('test name')
+    def test_set_name(self, result_item, builder):
+        result_item.set_name('test name')
+        builder.get_object.return_value.set_text.assert_called_with('test name')
 
-    @mock.patch.object(ResultItem, 'get_toplevel')
-    def test_on_click(self, mock_get_toplevel):
-        mock_get_toplevel.return_value = mock.MagicMock()
-        self.result_item.set_index(3)
-        self.result_item.on_click(None, None)
+    def test_on_click(self, mocker, result_item):
+        mock_get_toplevel = mocker.patch.object(result_item, 'get_toplevel')
+
+        result_item.set_index(3)
+        result_item.on_click(None, None)
         mock_get_toplevel.return_value.select_result_item.assert_called_with(3)
         assert mock_get_toplevel.return_value.enter_result_item.called
 
-    @mock.patch.object(ResultItem, 'get_toplevel')
-    def test_on_mouse_hover(self, mock_get_toplevel):
-        mock_get_toplevel.return_value = mock.MagicMock()
-        self.result_item.set_index(4)
-        self.result_item.on_mouse_hover(None, None)
+    def test_on_mouse_hover(self, mocker, result_item):
+        mock_get_toplevel = mocker.patch.object(result_item, 'get_toplevel')
+
+        result_item.set_index(4)
+        result_item.on_mouse_hover(None, None)
         mock_get_toplevel.return_value.select_result_item.assert_called_with(4)
 
-    def test_set_description(self):
-        self.result_item.set_description('test description')
-        self.builderMock.get_object.return_value.set_text.assert_called_with('test description')
+    def test_set_description(self, result_item, builder):
+        result_item.set_description('test description')
+        builder.get_object.return_value.set_text.assert_called_with('test description')
 
-    def test_no_description(self):
-        self.result_item.set_description(None)
-        self.builderMock.get_object.return_value.destroy.assert_called_with()
+    def test_no_description(self, result_item, builder):
+        result_item.set_description(None)
+        builder.get_object.return_value.destroy.assert_called_with()
 
-    def test_set_shortcut(self):
-        self.result_item.set_shortcut('Alt+1')
-        self.builderMock.get_object.return_value.set_text.assert_called_with('Alt+1')
+    def test_set_shortcut(self, result_item, builder):
+        result_item.set_shortcut('Alt+1')
+        builder.get_object.return_value.set_text.assert_called_with('Alt+1')
