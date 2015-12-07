82b7e6b640f6dfb84b8f4e68219c0b629070af39
Aleksandr Gornostal <sanya.gornostal@gmail.com>
2015-12-07 22:11:26 +0200

    #35 - updated gulp file to build to the same path for dev and prod

--- data/preferences/gulpfile.js
+++ data/preferences/gulpfile.js
@@ -1,11 +1,17 @@
 var gulp          = require('gulp');
 var concat        = require('gulp-concat');
-var uglify        = require('gulp-uglify');
-var minifyCss     = require('gulp-minify-css');
 var sass          = require('gulp-sass');
 var templateCache = require('gulp-angular-templatecache');
 var browserSync   = require('browser-sync').create();
 var inject        = require('gulp-inject');
+var del           = require('del');
+var childProc     = require('child_process');
+
+var bowerCss = ['bower_components/pure/pure-min.css',
+                'bower_components/font-awesome/css/font-awesome.min.css'];
+
+var bowerJs = ['bower_components/angular/angular.min.js',
+               'bower_components/angular-route/angular-route.min.js'];
 
 // Tasks for dev environment
 // Static Server + watching scss/html/js files
@@ -25,10 +31,14 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("src/components/**/*.js").on('change', browserSync.reload);
 });
 
+gulp.task('copy-index-html', function() {
+  childProc.execSync('cp index.tmpl.html index.html');
+});
+
 gulp.task('sass', function() {
-  return gulp.src("./src/components/**/*.scss")
+  return gulp.src("src/components/**/*.scss")
     .pipe(sass())
-    .pipe(gulp.dest("./.tmp/css/"))
+    .pipe(gulp.dest(".tmp/css/"))
     .pipe(browserSync.stream());
 });
 
@@ -40,39 +50,46 @@ gulp.task('compile-templates', function() {
     .pipe(gulp.dest('.tmp/templates/'));
 });
 
-gulp.task('inject-dev',['sass', 'compile-templates'], function() {
-  var target = gulp.src('./index.html'),
-    sources = gulp.src(['./src/js/libs/jsonp.js', './src/**/*.js',
-                        './.tmp/templates/*.js', './.tmp/css/**/*.css'], {read: false});
+gulp.task('inject-dev',['copy-index-html', 'sass', 'compile-templates'], function() {
+  var target = gulp.src('index.html');
+  var devFiles = ['src/**/*.js', '.tmp/templates/*.js', '.tmp/css/**/*.css'];
+  var sources = gulp.src(bowerCss.concat(bowerJs).concat(devFiles), {read: false});
 
    target.pipe(inject(sources, {addRootSlash: false}))
       .pipe(gulp.dest('./'));
 });
 
-// Tasks for producion environment
-gulp.task('concat-css', ['sass'], function() {
-  return gulp.src('./.tmp/css/**/*.css')
-    .pipe(minifyCss())
-    .pipe(concat('main.min.css'))
-    .pipe(gulp.dest('prod/css'));
+// Tasks for a production build
+
+gulp.task('clean-build', function () {
+  del.sync(['build', '.tmp']);
 });
 
-gulp.task('concat-js', ['compile-templates'], function() {
-  return gulp.src(['./src/components/**/*.js', './.tmp/templates/*.js'])
+gulp.task('build-concat-css', ['sass'], function() {
+  return gulp.src(bowerCss.concat(['.tmp/css/**/*.css']))
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('build-concat-js', ['compile-templates'], function() {
+  return gulp.src(bowerJs.concat(['src/**/*.js', '.tmp/templates/*.js']))
     .pipe(concat('app.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('prod/js'));
+    .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('inject-prod',['concat-css', 'concat-js'], function() {
-  var target = gulp.src('./index.html'),
-    sources = gulp.src(['./src/js/libs/jsonp.js', './prod/**/*.js',
-                        './prod/css/**/*.css'], {read: false});
+gulp.task('inject-build',['copy-index-html', 'build-concat-css', 'build-concat-js'], function() {
+  var target = gulp.src('index.html'),
+    sources = gulp.src(['build/**/*.js', 'build/css/**/*.css'], {read: false});
 
    target.pipe(inject(sources, {addRootSlash: false}))
-      .pipe(gulp.dest('./prod'));
+      .pipe(gulp.dest('./'));
+});
+
+gulp.task('build-copy-fonts', function() {
+  return gulp.src('bower_components/font-awesome/fonts/*')
+    .pipe(gulp.dest('build/fonts'));
 });
 
 
-gulp.task('default', ['serve', 'compile-templates', 'inject-dev']);
-gulp.task('build', ['inject-prod']);
+gulp.task('default', ['serve', 'inject-dev']);
+gulp.task('build', ['clean-build', 'inject-build', 'build-copy-fonts']);
