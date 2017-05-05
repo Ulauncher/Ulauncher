4aad67013d03a23a2d7d35f75c08b0ac00101100
Aleksandr Gornostal <sanya.gornostal@gmail.com>
2017-05-05 23:29:31 +0300

    updated dev-run.sh [skip ci]

--- dev-run.sh
+++ dev-run.sh
@@ -3,13 +3,6 @@
 echo "installing media files to ~/.local to be able to load icons by name"
 python setup.py install_data --install-dir="$HOME/.local" || exit 1
 
-pushd "data/preferences"
-if ! hash git 2>/dev/null || ! grep -q `git rev-parse HEAD` ".build-checksum"; then
-    echo "building preferences UI if it's not built"
-    npm i && ./node_modules/bower/bin/bower install --config.interactive=false && ./gulp build || exit 1
-fi
-popd
-
 echo "adding .desktop file"
 mkdir -p ~/.local/share/applications/
 desktop=~/.local/share/applications/ulauncher.desktop
