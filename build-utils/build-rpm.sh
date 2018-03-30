e867bff150e6eab7bb41def84dd9f432c569f61b
Aleksandr Gornostal <al.gornostal@gmail.com>
2018-03-30 22:06:48 +0300

    Issue #135: Added build step for CentOS 7

--- build-utils/build-rpm.sh
+++ build-utils/build-rpm.sh
@@ -58,3 +58,9 @@ sed -i "s/\[bdist_rpm\]/[bdist_rpm_fedora]/g" setup.cfg
 sed -i "s/\[bdist_rpm_suse\]/[bdist_rpm]/g" setup.cfg
 python setup.py bdist_rpm
 find . -name "*noarch.rpm" -print0 | xargs -0 -I file cp file /tmp/ulauncher_$1_suse.rpm
+
+# build for CentOS 7
+sed -i "s/\[bdist_rpm\]/[bdist_rpm_suse]/g" setup.cfg
+sed -i "s/\[bdist_rpm_centos7\]/[bdist_rpm]/g" setup.cfg
+python setup.py bdist_rpm
+find . -name "*noarch.rpm" -print0 | xargs -0 -I file cp file /tmp/ulauncher_$1_centos7.rpm
