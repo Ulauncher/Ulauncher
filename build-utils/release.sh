472400f1fd1974b84cc4e95c3b72fded22b1bf5c
Aleksandr Gornostal <al.gornostal@gmail.com>
2018-04-28 16:22:05 +0300

    use only FTP_PROXY

--- build-utils/release.sh
+++ build-utils/release.sh
@@ -85,8 +85,6 @@ launchpad_upload() {
 
     docker run \
         --rm \
-        -e HTTP_PROXY \
-        -e HTTPS_PROXY \
         -e FTP_PROXY \
         -v $(pwd):/root/ulauncher \
         $BUILD_IMAGE \
