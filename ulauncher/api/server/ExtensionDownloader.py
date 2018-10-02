5db91e892d601a85e95b72453aa0a515eece5ca8
Aleksandr Gornostal <al.gornostal@gmail.com>
2018-10-02 12:25:04 +0300

    #292 - Improved error logging for extensions

--- ulauncher/api/server/ExtensionDownloader.py
+++ ulauncher/api/server/ExtensionDownloader.py
@@ -56,7 +56,7 @@ class ExtensionDownloader(object):
         try:
             gh_commit = gh_ext.get_last_commit()
         except Exception as e:
-            logger.error('gh_ext.get_ext_meta() failed. %s: %s' % (type(e).__name__, e.message))
+            logger.error('gh_ext.get_ext_meta() failed. %s: %s' % (type(e).__name__, e))
             raise InvalidGithubUrlError('Project is not available on Github')
 
         filename = download_zip(gh_ext.get_download_url(gh_commit['last_commit']))
@@ -85,7 +85,7 @@ class ExtensionDownloader(object):
                 ext_id = self.download(ext['url'])
                 self.ext_runner.run(ext_id)
             except Exception as e:
-                logger.error('%s: %s' % (type(e).__name__, e.message))
+                logger.error('%s: %s' % (type(e).__name__, e))
 
     def remove(self, ext_id):
         try:
