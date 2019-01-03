0c186b0a3008ad4f35bc08209e6e4db5a827e6a4
Aleksandr Gornostal <al.gornostal@gmail.com>
2019-01-03 10:18:22 +0200

    Fixed syntax error

--- ulauncher/util/decorator/singleton.py
+++ ulauncher/util/decorator/singleton.py
@@ -1,12 +1,10 @@
-from typing import Dict, Callable, TypeVar
+from typing import Dict, Callable, Any
 
-T = TypeVar('T')
-DecoratedFn = Callable[..., T]
-CachedInstances = Dict[DecoratedFn, T]
+CachedInstances = Dict[Callable, Any]
 objects = {}  # type: CachedInstances
 
 
-def singleton(fn: DecoratedFn) -> DecoratedFn:
+def singleton(fn):
     """
     Decorator function.
     Call to a decorated function always returns the same instance
@@ -14,7 +12,7 @@ def singleton(fn: DecoratedFn) -> DecoratedFn:
     Note: it doesn't take into account args and kwargs when looks up a saved instance
     Call a decorated function with `spawn=True` in order to get a new instance
     """
-    def wrapper(*args, **kwargs) -> T:
+    def wrapper(*args, **kwargs):
         if not kwargs.get('spawn') and objects.get(fn):
             return objects[fn]
 
