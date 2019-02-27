7dda2fd6bc6b2879ebea50042f96ad73d2ac6d15
Aleksandr Gornostal <al.gornostal@gmail.com>
2019-02-27 07:53:17 +0200

    Fixed release.sh

--- build-utils/release.sh
+++ build-utils/release.sh
@@ -67,7 +67,7 @@ aur_update() {
     # Push new PKGBUILD to AUR stable channel
     docker run \
         --rm \
-        -v $(pwd):/root/ulauncher \
+        -v $(pwd):/home/kenny/ulauncher \
         $ARCH_BUILD_IMAGE \
         bash -c "UPDATE_STABLE=1 ./build-utils/aur-update.py $VERSION"
 }
