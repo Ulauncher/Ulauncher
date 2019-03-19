9d80ca57cf41ec516304b1c3a623246723a3def2
Sagar Tewari <sagartewari01@gmail.com>
2019-03-19 16:31:00 +0530

    added trigonometric functions to calc mode

--- ulauncher/search/calc/CalcMode.py
+++ ulauncher/search/calc/CalcMode.py
@@ -1,4 +1,5 @@
 import re
+import math
 import ast
 import operator as op
 
@@ -6,12 +7,30 @@ from ulauncher.api.shared.action.RenderResultListAction import RenderResultListA
 from ulauncher.search.BaseSearchMode import BaseSearchMode
 from ulauncher.search.calc.CalcResultItem import CalcResultItem
 
-
 # supported operators
 operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
              ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
              ast.USub: op.neg}
 
+functions = {
+    'sin': math.sin,
+    'cos' : math.cos,
+    'tan' : math.tan,
+    'asin': math.asin,
+    'acos' : math.acos,
+    'atan' : math.atan,
+    'cot' : lambda a : 1/math.tan(a),
+    'sec' : lambda a : 1/math.cos(a),
+    'cosec' : lambda a : 1/math.sin(a),
+    'log' : math.log,
+    'log10' : math.log10,
+    'r' : lambda a : a*math.pi/180
+}
+
+constants = {
+    'pi' : math.pi,
+    'e' : math.e
+}
 
 def eval_expr(expr):
     """
@@ -26,11 +45,12 @@ def eval_expr(expr):
     """
     try:
         return _eval(ast.parse(expr, mode='eval').body)
-    # pylint: disable=broad-except
-    except Exception:
+    except SyntaxError:
         # if failed, try without the last symbol
-        return _eval(ast.parse(expr[:-1], mode='eval').body)
-
+        try:
+            return _eval(ast.parse(expr[:-1], mode='eval').body)
+        except SyntaxError:
+            raise ValueError()
 
 def _eval(node):
     if isinstance(node, ast.Num):  # <number>
@@ -39,17 +59,29 @@ def _eval(node):
         return operators[type(node.op)](_eval(node.left), _eval(node.right))
     if isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
         return operators[type(node.op)](_eval(node.operand))
-
-    raise TypeError(node)
-
+    elif isinstance(node, ast.Name):
+        return constants[node.id]
+    elif isinstance(node, ast.Call):
+        if len(node.args) != 1:
+            raise TypeError(node)
+        return functions[node.func.id](_eval(node.args[0]))
+    else:
+        raise TypeError(node)
 
 class CalcMode(BaseSearchMode):
     RE_CALC = re.compile(r'^[\d\-\(\.][\d\*+\/\-\.e\(\)\^ ]*$', flags=re.IGNORECASE)
 
     def is_enabled(self, query):
-        return re.match(self.RE_CALC, query)
+        try:
+            res = eval_expr(query)
+        except ArithmeticError:
+            return True
+        except Exception as e:
+            return False
+        return True
 
     def handle_query(self, query):
+        error_msg = 'Invalid expression'
         try:
             result = eval_expr(query)
             if result is None:
