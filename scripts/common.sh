5dae90574ceec209304e66b8bb15aefd9f714d1d
Aleksandr Gornostal <al.gornostal@gmail.com>
2019-05-26 21:38:45 +0300

    Fixed bash colors

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # define fonts
-dim=$(tput dim)
-normal=$(tput sgr0)
-reset=$(tput sgr0)
-cyan=$'\e[94m'
+normal=$'\e[0m'
+reset=$normal
+bold=$'\e[1m'
+dim=$'\e[2m'
+underline=$'\e[4m'
+red=$'\e[31m'
+green=$'\e[32m'
 yellow=$'\e[33m'
-bold=$(tput bold)
-underline=$(tput sgr 0 1)
-red=$(tput setaf 1)
-green=$(tput setaf 76)
-white=$(tput setaf 7)
-tan=$(tput setaf 202)
-blue=$(tput setaf 25)
+blue=$'\e[34m'
+tan=$'\e[91m'
+cyan=$'\e[96m'
+white=$'\e[97m'
 
 BUILD_IMAGE=ulauncher/build-image:5.0
 FEDORA_28_BUILD_IMAGE=ulauncher/fedora:5.0-28
@@ -23,7 +23,7 @@ underline() {
     printf "${underline}${bold}%s${reset}\n" "$@"
 }
 h1() {
-    printf "\n${underline}${bold}${blue}%s${reset}\n" "$@"
+    printf "\n${underline}${bold}${cyan}%s${reset}\n" "$@"
 }
 h2() {
     printf "\n${underline}${bold}${white}%s${reset}\n" "$@"
@@ -38,10 +38,10 @@ success() {
     printf "${green}✔ %s${reset}\n" "$@"
 }
 error() {
-    printf "${red}✖ %s${reset}\n" "$@"
+    printf "${tan}✖ %s${reset}\n" "$@"
 }
 warn() {
-    printf "${tan}➜ %s${reset}\n" "$@"
+    printf "${yellow}➜ %s${reset}\n" "$@"
 }
 bold() {
     printf "${bold}%s${reset}\n" "$@"
