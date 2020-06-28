47abb560dbbbc24f520777c0b8add01997f837ad
Aleksandr Gornostal <al.gornostal@gmail.com>
2020-06-28 20:19:04 +0300

    Added a unit test

---
+++ tests/utils/test_version_cmp.py
@@ -0,0 +1,9 @@
+from ulauncher.utils.version_cmp import gtk_version_is_gte
+
+
+def test_gtk_version_is_gte():
+    # test sould pass with GTK 3.22.30
+    assert gtk_version_is_gte(1, 99, 100)
+    assert not gtk_version_is_gte(100, 1, 1)
+    assert not gtk_version_is_gte(3, 0, 0)
+    assert not gtk_version_is_gte(3, 22, 0)
