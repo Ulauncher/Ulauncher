86b12c9be2a3a21ebbed1bbc0fddd45d87ddadec
Albin Larsson <mail@albinlarsson.com>
2021-08-09 18:28:27 +0200

    Fix version_cmp test

--- tests/utils/test_version_cmp.py
+++ tests/utils/test_version_cmp.py
@@ -5,5 +5,5 @@ def test_gtk_version_is_gte():
     # test sould pass with GTK 3.22.30
     assert gtk_version_is_gte(1, 99, 100)
     assert not gtk_version_is_gte(100, 1, 1)
-    assert not gtk_version_is_gte(3, 0, 0)
-    assert not gtk_version_is_gte(3, 22, 0)
+    assert gtk_version_is_gte(3, 0, 0)
+    assert gtk_version_is_gte(3, 22, 0)
