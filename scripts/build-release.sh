371fbd2db0b42e63dd307183eee0c053aa0126fd
Albin Larsson <mail@albinlarsson.com>
2021-08-24 14:56:24 +0200

    Restore deploy uploads

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -21,6 +21,12 @@ build-release() {
 
     # RPMs deactivated for now
     # create_rpms
+
+    # Upload if tag doesn't contain "test"
+    if [[ $(echo "$VERSION" | tr '[:upper:]' '[:lower:]') != *test* ]]; then
+        launchpad_upload
+        aur_update
+    fi
 }
 
 create_deb() {
