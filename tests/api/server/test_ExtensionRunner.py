0886ce6068aa016aed74e278abbad94a558e6635
Troy Curtis Jr <troy@troycurtisjr.com>
2021-10-31 14:59:06 -0400

    Use Gio.SubprocessLauncher instead of custom threads for extension running.

--- tests/api/server/test_ExtensionRunner.py
+++ tests/api/server/test_ExtensionRunner.py
@@ -1,6 +1,8 @@
 import mock
 import pytest
-from ulauncher.api.server.ExtensionRunner import ExtensionRunner, ExtRunErrorName
+import signal
+
+from ulauncher.api.server.ExtensionRunner import ExtensionRunner, ExtRunErrorName, ExtensionIsNotRunningError
 from ulauncher.api.server.ExtensionServer import ExtensionServer
 from ulauncher.api.server.ExtensionManifest import ExtensionManifestError
 from ulauncher.api.shared.errors import ErrorName
@@ -10,7 +12,9 @@ class TestExtensionRunner:
 
     @pytest.fixture
     def runner(self, ext_server):
-        return ExtensionRunner(ext_server)
+        thisrunner = ExtensionRunner(ext_server)
+        thisrunner.dont_run_extensions = False
+        return thisrunner
 
     @pytest.fixture
     def ext_server(self):
@@ -19,6 +23,10 @@ class TestExtensionRunner:
     @pytest.fixture(autouse=True)
     def find_extensions(self, mocker):
         return mocker.patch('ulauncher.api.server.ExtensionRunner.find_extensions')
+
+    @pytest.fixture(autouse=True)
+    def timer(self, mocker):
+        return mocker.patch('ulauncher.api.server.ExtensionRunner.timer')
 
     @pytest.fixture(autouse=True)
     def get_options(self, mocker):
@@ -32,9 +40,21 @@ class TestExtensionRunner:
     def manifest(self, ExtensionManifest):
         return ExtensionManifest.open.return_value
 
-    @pytest.fixture(autouse=True)
-    def run_async(self, mocker):
-        return mocker.patch('ulauncher.api.server.ExtensionRunner.run_async')
+    @pytest.fixture
+    def SubprocessLauncher(self, mocker):
+        return mocker.patch('ulauncher.api.server.ExtensionRunner.Gio.SubprocessLauncher')
+
+    @pytest.fixture
+    def DataInputStream(self, mocker):
+        return mocker.patch('ulauncher.api.server.ExtensionRunner.Gio.DataInputStream')
+
+    @pytest.fixture
+    def ProcessErrorExtractor(self, mocker):
+        return mocker.patch('ulauncher.api.server.ExtensionRunner.ProcessErrorExtractor')
+
+    @pytest.fixture
+    def time(self, mocker):
+        return mocker.patch('ulauncher.api.server.ExtensionRunner.time')
 
     def test_run__incompatible_version__exception_is_raised(self, runner, manifest):
         manifest.check_compatibility.side_effect = ExtensionManifestError(
@@ -42,9 +62,14 @@ class TestExtensionRunner:
         with pytest.raises(ExtensionManifestError):
             runner.run('id')
 
-    def test_run__ExtensionManifest_open__is_called(self, runner, ExtensionManifest):
-        runner.run('id')
-        ExtensionManifest.open.assert_called_with('id')
+    def test_run__basic_execution__is_called(self, runner, ExtensionManifest, SubprocessLauncher, DataInputStream):
+        extid = 'id'
+        runner.run(extid)
+        ExtensionManifest.open.assert_called_with(extid)
+        SubprocessLauncher.new.assert_called_once()
+        extproc = runner.extension_procs[extid]
+        extproc.subprocess.wait_async.assert_called_once()
+        extproc.error_stream.read_line_async.assert_called_once()
 
     def test_run_all__run__called_with_extension_ids(self, runner, mocker, find_extensions):
         mocker.patch.object(runner, 'run')
@@ -59,3 +84,102 @@ class TestExtensionRunner:
         error = runner.get_extension_error('id_1')
         assert error['name'] == ExtRunErrorName.Terminated.value
         assert error['message'] == 'message'
+
+    def test_read_stderr_line(self, runner, DataInputStream):
+        test_output1 = b"Test Output 1"
+        test_output2 = b"Test Output 2"
+        extid = "id"
+        read_line_finish = mock.Mock()
+
+        runner.run(extid)
+        extproc = runner.extension_procs[extid]
+        extproc.error_stream.read_line_finish = read_line_finish
+
+        read_line_finish.return_value = (test_output1, len(test_output1))
+        runner.handle_stderr(extproc.error_stream, mock.Mock(), extid)
+        # Confirm the output is stored in recent_errors and read_line_async is called for the next
+        # line.
+        assert extproc.recent_errors[0] == test_output1
+        assert extproc.error_stream.read_line_async.call_count == 2
+
+        read_line_finish.return_value = (test_output2, len(test_output2))
+        runner.handle_stderr(extproc.error_stream, mock.Mock(), extid)
+        # The latest line should replace the previous line
+        assert extproc.recent_errors[0] == test_output2
+        assert extproc.error_stream.read_line_async.call_count == 3
+
+    def test_handle_wait__signaled(self, runner, DataInputStream, SubprocessLauncher):
+        extid = "id"
+
+        runner.run(extid)
+        extproc = runner.extension_procs[extid]
+        extproc.subprocess.get_if_signaled.return_value = True
+        extproc.subprocess.get_term_sig.return_value = 9
+        # Check pre-condition
+        assert extid not in runner.extension_errors
+
+        runner.handle_wait(extproc.subprocess, mock.Mock(), extid)
+        # Confirm error handling
+        assert extid in runner.extension_errors
+        assert extid not in runner.extension_procs
+
+    def test_handle_wait__rapid_exit(self, runner, DataInputStream, SubprocessLauncher, time, ProcessErrorExtractor):
+        extid = "id"
+        curtime = 100.0
+        starttime = curtime - 0.5
+        time.return_value = starttime
+
+        runner.run(extid)
+        extproc = runner.extension_procs[extid]
+        extproc.subprocess.get_if_signaled.return_value = False
+        extproc.subprocess.get_exit_status.return_value = 9
+        time.return_value = curtime
+
+        ProcessErrorExtractor.is_import_error.return_value = True
+        ProcessErrorExtractor.get_missing_package_name.return_value = "TestPackage"
+        runner.handle_wait(extproc.subprocess, mock.Mock(), extid)
+        assert extid in runner.extension_errors
+        assert extid not in runner.extension_procs
+
+    def test_handle_wait__restart(self, runner, DataInputStream, SubprocessLauncher, time):
+        extid = "id"
+        curtime = 100.0
+        starttime = curtime - 5
+        time.return_value = starttime
+
+        runner.run(extid)
+        extproc = runner.extension_procs[extid]
+        extproc.subprocess.get_if_signaled.return_value = False
+        extproc.subprocess.get_exit_status.return_value = 9
+        time.return_value = curtime
+
+        runner.run = mock.Mock()
+        runner.handle_wait(extproc.subprocess, mock.Mock(), extid)
+        assert extid in runner.extension_errors
+        # run() is mocked, so the ExtensionProce won't get readded after "restart"
+        assert extid not in runner.extension_procs
+        runner.run.assert_called_once()
+
+    def test_stop(self, runner, timer, SubprocessLauncher, DataInputStream):
+        extid = "id"
+
+        with pytest.raises(ExtensionIsNotRunningError):
+            runner.stop(extid)
+
+        runner.run(extid)
+        extproc = runner.extension_procs[extid]
+        runner.stop(extid)
+        assert extid not in runner.extension_procs
+        extproc.subprocess.send_signal.assert_called_with(signal.SIGTERM)
+        timer.assert_called_once()
+
+    def test_confirm_termination(self, runner):
+        extproc = mock.Mock()
+        extproc.subprocess.get_identifier.return_value = None
+
+        runner.confirm_termination(extproc)
+        extproc.subprocess.send_signal.assert_not_called()
+
+        extproc.subprocess.get_identifier.return_value = 1
+        runner.confirm_termination(extproc)
+        extproc.subprocess.send_signal.assert_called_with(signal.SIGKILL)
