ed3f462b5794d1f142051204f5ef19dad6a52e8e
Troy Curtis Jr <troy@troycurtisjr.com>
2021-10-31 14:47:44 -0400

    Support a failing `git describe` when looking for a version.
    - Remove the self-referencing `./setup.py --version` in __init__.py.

--- ulauncher/__init__.py
+++ ulauncher/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
 # This file is overwritten by the build_wrapper script in setup.py
 # IF YOU EDIT THIS FILE make sure your changes are reflected there
@@ -8,10 +8,10 @@ from subprocess import check_output
 def _exec_get_(*args):
     try:
         return check_output(list(args)).decode('utf-8').rstrip()
-    except FileNotFoundError:
+    except (FileNotFoundError, CalledProcessError):
         return ""
 
 
 __data_directory__ = f'{Path(__file__).resolve().parent.parent}/data'
 __is_dev__ = True
-__version__ = _exec_get_("git", "describe", "--tags") or _exec_get_("./setup.py", "--version") or "DEV"
+__version__ = _exec_get_("git", "describe", "--tags") or "DEV"
