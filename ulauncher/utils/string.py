6429abb9b1c557d266edde1e4292f8c084ded29a
Albin Larsson <mail@albinlarsson.com>
2021-11-09 23:38:03 +0100

    Add string.remove_accents for searching

--- ulauncher/utils/string.py
+++ ulauncher/utils/string.py
@@ -1,4 +1,5 @@
 import re
+import unicodedata
 
 _first_cap_re = re.compile('(.)([A-Z][a-z]+)')
 _all_cap_re = re.compile('([a-z0-9])([A-Z])')
@@ -7,3 +8,7 @@ _all_cap_re = re.compile('([a-z0-9])([A-Z])')
 def split_camel_case(text: str, sep: str = '_') -> str:
     s1 = _first_cap_re.sub(r'\1%s\2' % sep, text)
     return _all_cap_re.sub(r'\1%s\2' % sep, s1).lower()
+
+
+def remove_accents(str):
+    return unicodedata.normalize('NFD', str).encode('ascii', 'ignore').decode('utf-8')
