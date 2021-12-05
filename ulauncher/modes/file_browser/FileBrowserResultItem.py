0ed869ce8815e2e527c0f339c3c9b87b6a74c37e
Albin Larsson <mail@albinlarsson.com>
2021-12-05 19:50:27 +0100

    Move utils.icon.get_file_icon to FileBrowserResultItem method

--- ulauncher/modes/file_browser/FileBrowserResultItem.py
+++ ulauncher/modes/file_browser/FileBrowserResultItem.py
@@ -1,15 +1,32 @@
 import os
+import mimetypes
+import gi
+gi.require_version('GLib', '2.0')
+# pylint: disable=wrong-import-position
+from gi.repository import GLib
 
 from ulauncher.api.shared.action.OpenAction import OpenAction
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.SetUserQueryAction import SetUserQueryAction
 from ulauncher.api.shared.item.SmallResultItem import SmallResultItem
 from ulauncher.utils.Path import Path
-from ulauncher.utils.icon import get_file_icon
+from ulauncher.utils.icon import load_icon
 
 from ulauncher.modes.file_browser.FileQueries import FileQueries
 from ulauncher.modes.file_browser.alt_menu.CopyPathToClipboardItem import CopyPathToClipboardItem
 from ulauncher.modes.file_browser.alt_menu.OpenFolderItem import OpenFolderItem
+
+SPECIAL_DIRS = {
+    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOWNLOAD): 'folder-download',
+    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOCUMENTS): 'folder-documents',
+    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_MUSIC): 'folder-music',
+    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PICTURES): 'folder-pictures',
+    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PUBLIC_SHARE): 'folder-publicshare',
+    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_TEMPLATES): 'folder-templates',
+    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_VIDEOS): 'folder-videos',
+    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DESKTOP): 'user-desktop',
+    os.path.expanduser('~'): 'folder-home'
+}
 
 
 class FileBrowserResultItem(SmallResultItem):
@@ -32,8 +49,21 @@ class FileBrowserResultItem(SmallResultItem):
         query = os.path.basename(query)
         return super().get_name_highlighted(query, color)
 
+    def get_file_icon(self):
+        if self.path.is_dir():
+            return SPECIAL_DIRS.get(str(self.path)) or "folder"
+
+        mime = mimetypes.guess_type(self.path.get_basename())[0]
+        if mime:
+            return mime.replace("/", "-")
+
+        if self.path.is_exe():
+            return "application-x-executable"
+
+        return "unknown"
+
     def get_icon(self):
-        return get_file_icon(self.path, self.get_icon_size())
+        return load_icon(self.get_file_icon(), self.get_icon_size())
 
     def on_enter(self, query):
         self._file_queries.save_query(self.path.get_abs_path())
--- ulauncher/utils/icon.py
+++ ulauncher/utils/icon.py
@@ -1,33 +1,15 @@
-import os
 import logging
-import mimetypes
 from functools import lru_cache
 
 import gi
-gi.require_version('GLib', '2.0')
 gi.require_version('Gtk', '3.0')
-gi.require_version('Gio', '2.0')
 gi.require_version('GdkPixbuf', '2.0')
-
 # pylint: disable=wrong-import-position
-from gi.repository import Gtk, GLib, GdkPixbuf
+from gi.repository import Gtk, GdkPixbuf
 from ulauncher.config import get_data_file
-
 
 icon_theme = Gtk.IconTheme.get_default()
 logger = logging.getLogger(__name__)
-
-SPECIAL_DIRS = {
-    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOWNLOAD): 'folder-download',
-    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOCUMENTS): 'folder-documents',
-    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_MUSIC): 'folder-music',
-    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PICTURES): 'folder-pictures',
-    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PUBLIC_SHARE): 'folder-publicshare',
-    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_TEMPLATES): 'folder-templates',
-    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_VIDEOS): 'folder-videos',
-    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DESKTOP): 'user-desktop',
-    os.path.expanduser('~'): 'folder-home'
-}
 
 
 @lru_cache(maxsize=50)
@@ -44,32 +26,3 @@ def load_icon(icon, size):
     except Exception as e:
         logger.info('Could not load icon %s. E: %s', icon, e)
         return load_icon(get_data_file('icons', 'executable.png'), size)
-
-
-def _get_themed_icon_by_name(icon_name, icon_size):
-    return icon_theme.load_icon(icon_name, icon_size, Gtk.IconLookupFlags.FORCE_SIZE)
-
-
-def get_file_icon(path, icon_size):
-    """
-    :param ~ulauncher.utils.Path.Path path:
-    :param int icon_size:
-    """
-    # pylint: disable=broad-except
-    try:
-        if path.is_dir():
-            special_dir = SPECIAL_DIRS.get(str(path))
-            if special_dir:
-                return _get_themed_icon_by_name(special_dir, icon_size)
-            return _get_themed_icon_by_name('folder', icon_size)
-
-        mime = mimetypes.guess_type(path.get_basename())[0]
-        if mime:
-            return _get_themed_icon_by_name(mime.replace('/', '-'), icon_size)
-
-        if path.is_exe():
-            return _get_themed_icon_by_name("application-x-executable", icon_size)
-    except Exception as e:
-        logger.warning('Icon not found %s. %s: %s', path, type(e).__name__, e)
-
-    return _get_themed_icon_by_name("unknown", icon_size)
