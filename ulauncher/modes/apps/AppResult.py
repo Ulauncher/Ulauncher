403c7436ea9a35b9898d4c881c87d60631d89fbb
Troy Curtis Jr <troy@troycurtisjr.com>
2021-12-14 21:00:24 -0500

    Make the linter happy.

--- ulauncher/modes/apps/AppResult.py
+++ ulauncher/modes/apps/AppResult.py
@@ -26,9 +26,9 @@ class AppResult(Result):
         # get_executable uses Exec, which is always specified, but it will return the actual executable.
         # Sometimes the actual executable is not the app to start, but a wrappers like "env" or "sh -c"
         self._executable = basename(
-            app_info.get_string('TryExec')
-            or app_info.get_executable()
-            or ""
+            app_info.get_string('TryExec') or
+            app_info.get_executable() or
+            ""
         )
         self._app_info = app_info
         self._query_history = QueryHistoryDb.get_instance()
