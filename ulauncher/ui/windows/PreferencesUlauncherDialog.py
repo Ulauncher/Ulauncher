1706ef3dfad007aea5b16287082e5f087b37f367
Albin Larsson <mail@albinlarsson.com>
2022-01-12 03:06:56 +0100

    Fix typo 'initalizing'

--- ulauncher/ui/windows/PreferencesUlauncherDialog.py
+++ ulauncher/ui/windows/PreferencesUlauncherDialog.py
@@ -94,7 +94,7 @@ class PreferencesUlauncherDialog(Gtk.Dialog, WindowHelper):
     def finish_initializing(self, builder):
         """Called while initializing this instance in __new__
 
-        finish_initalizing should be called after parsing the ui definition
+        finish_initializing should be called after parsing the ui definition
         and creating a PreferencesDialog object with it in order to
         finish initializing the start of the new PerferencesUlauncherDialog
         instance.
