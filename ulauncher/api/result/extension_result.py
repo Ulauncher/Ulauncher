e16d2636bd694bb87166fcce338a61188f71ee17
Albin Larsson <mail@albinlarsson.com>
2022-01-17 01:43:07 +0100

    Restore support for relative paths in ExtensionResult.icon

--- ulauncher/api/result/extension_result.py
+++ ulauncher/api/result/extension_result.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from ulauncher.api.result.searchable_result import SearchableResult
 from ulauncher.api.shared.action.BaseAction import BaseAction
 
@@ -12,6 +14,10 @@ class ExtensionResult(SearchableResult):
 
     def __init__(self, *args, **kw):
         super().__init__(*args, **kw)
+        self.extension_path = os.path.dirname(sys.argv[0])
+
+        if isinstance(self.icon, str) and not self.icon.startswith('/') and "." in self.icon:
+            self.icon = os.path.join(self.extension_path, self.icon)
 
         if self._on_enter and not isinstance(self._on_enter, BaseAction):
             raise Exception("Invalid on_enter argument. Expected BaseAction")
