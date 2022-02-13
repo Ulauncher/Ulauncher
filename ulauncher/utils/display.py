b9dce4a838b16572aabcba91d1ffde0bf4396c53
Albin Larsson <mail@albinlarsson.com>
2022-02-13 18:37:43 +0100

    Simplify Display utilities more (using newer GTK 3.22+ APIs)

--- ulauncher/utils/display.py
+++ ulauncher/utils/display.py
@@ -10,40 +10,30 @@ from gi.repository import Gdk, GdkX11, Gio
 
 logger = logging.getLogger(__name__)
 
-def get_screen_and_monitor(use_mouse_position):
+
+def get_monitor(use_mouse_position=False):
     """
-    Returns the default screen and an appropriate monitor.
+    :rtype: class:Gdk.Monitor
     """
-    default_screen = Gdk.Screen.get_default()
-    monitor_nr = default_screen.get_primary_monitor()
+    display = Gdk.Display.get_default()
 
     if use_mouse_position:
         try:
             x11_display = GdkX11.X11Display.get_default()
             seat = x11_display.get_default_seat()
             (_, x, y) = seat.get_pointer().get_position()
-            monitor_nr = default_screen.get_monitor_at_point(x, y)
+            return display.get_monitor_at_point(x, y)
         # pylint: disable=broad-except
         except Exception as e:
             logger.exception("Unexpected exception: %s", e)
 
-    return default_screen, monitor_nr
-
-
-def get_monitor_geometry(use_mouse_position=True):
-    """
-    :rtype int:
-    """
-    default_screen, monitor_nr = get_screen_and_monitor(use_mouse_position)
-
-    return default_screen.get_monitor_geometry(monitor_nr)
+    return display.get_primary_monitor() or display.get_monitor(0)
 
 
 def get_scaling_factor() -> int:
     # These two are rougly the same thing as the latter doesn't apply only to text
     # Text_scaling allow fractional scaling
     # GTK doesn't seem to allow different scaling factors on different displays
-    default_screen, monitor_nr = get_screen_and_monitor(False)
-    monitor_scaling = default_screen.get_monitor_scale_factor(monitor_nr)
+    monitor_scaling = get_monitor().get_scale_factor()
     text_scaling = Gio.Settings.new("org.gnome.desktop.interface").get_double('text-scaling-factor')
     return monitor_scaling * text_scaling
