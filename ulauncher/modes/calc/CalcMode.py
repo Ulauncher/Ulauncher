f5e4394453d8cdbba5705f6b431ebb2cfe46d108
Jan Henrik Müller <janhenrikm@yahoo.de>
2022-04-09 17:42:52 +0200

    Add math functions to calculator

--- ulauncher/modes/calc/CalcMode.py
+++ ulauncher/modes/calc/CalcMode.py
@@ -1,5 +1,6 @@
 import re
 import ast
+import math
 from decimal import Decimal
 import operator as op
 from functools import lru_cache
@@ -13,6 +14,16 @@ operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
              ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
              ast.USub: op.neg, ast.Mod: op.mod}
 
+functions = {"sqrt": Decimal.sqrt, "exp": Decimal.exp,
+             "ln": Decimal.ln, "log10": Decimal.log10,
+             "sin": math.sin, "cos": math.cos, "tan": math.tan,
+             "asin": math.asin, "acos": math.acos, "atan": math.atan,
+             "sinh": math.sinh, "cosh": math.cosh, "tanh": math.tanh,
+             "asinh": math.asinh, "acosh": math.acosh, "atanh": math.atanh,
+             "erf": math.erf, "erfc": math.erfc, "gamma": math.gamma,
+             "lgamma": math.lgamma}
+
+regex = r'^(?:[\d\*+\/\%\-\.,e\(\)\^ ]|' + "|".join(functions.keys()) + r')+$'
 
 # Show a friendlier output for incomplete queries, instead of "Invalid"
 def normalize_expr(expr):
@@ -55,12 +66,18 @@ def _eval(node):
         return operators[type(node.op)](_eval(node.left), _eval(node.right))
     if isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
         return operators[type(node.op)](_eval(node.operand))
+    if isinstance(node, ast.Call): # <func>(<args>)
+        if node.func.id in functions:
+            value = functions[node.func.id](_eval(node.args[0]))
+            if isinstance(value, float):
+                value = Decimal(value)
+            return value
 
     raise TypeError(node)
 
 
 class CalcMode(BaseMode):
-    RE_CALC = re.compile(r'^[\d\-\(\.,][\d\*+\/\%\-\.,e\(\)\^ ]*$', flags=re.IGNORECASE)
+    RE_CALC = re.compile(regex, flags=re.IGNORECASE)
 
     def is_enabled(self, query):
         return bool(re.match(self.RE_CALC, query))
