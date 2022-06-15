fa5a031a05fd8a9f68d2afa511c07eaca2ff6560
Albin Larsson <mail@albinlarsson.com>
2022-06-15 15:17:08 +0200

    Load Ulauncher version and description from setup.cfg for Sphinx

--- ulauncher/__init__.py
+++ ulauncher/__init__.py
@@ -4,11 +4,13 @@ from pathlib import Path
 # This file is overwritten by the build_wrapper script in setup.py
 # IF YOU EDIT THIS FILE make sure your changes are reflected there
 
+_PROJECT_ROOT = Path(__file__).resolve().parent.parent
 config = ConfigParser()
-config.read('setup.cfg')
+config.read(f"{_PROJECT_ROOT}/setup.cfg")
 
 
 # __assets_dir__ is by default `<ulauncher_path>/../data/` in trunk
 # and `/usr/share/ulauncher` in an installed version
-__assets_dir__ = f'{Path(__file__).resolve().parent.parent}/data'
-__version__ = config['metadata']['version']
+__assets_dir__ = f"{_PROJECT_ROOT}/data"
+__version__ = config["metadata"]["version"]
+__description__ = config["metadata"]["description"]
