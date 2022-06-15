7b98576fd142c840faf2b75e405216aa16804ebb
Albin Larsson <mail@albinlarsson.com>
2022-06-16 01:25:54 +0200

    Fix typo in Query

--- ulauncher/modes/Query.py
+++ ulauncher/modes/Query.py
@@ -1,16 +1,16 @@
 class Query(str):
     # Splits to a list with the keyword and argument and pad with empty strings
-    def _get_conponents(self):
+    def _get_components(self):
         components = self.split(None, 1)
         return components + [""] * (2 - len(components))
 
     @property
     def keyword(self):
-        return self._get_conponents()[0]
+        return self._get_components()[0]
 
     @property
     def argument(self):
-        return self._get_conponents()[1]
+        return self._get_components()[1]
 
     def get_keyword(self):
         return self.keyword
