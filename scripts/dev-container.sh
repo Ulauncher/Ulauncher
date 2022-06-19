e3d5d68ec2dfd702cac1ab95d47118e48d5e24a5
Albin Larsson <mail@albinlarsson.com>
2022-06-19 21:40:12 +0200

    dev-container: Check if either podman or docker exists and favor running with podman

--- scripts/dev-container.sh
+++ scripts/dev-container.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# Use podman (recommended) if it exists, else docker
+# Any files created with docker is then owned by root, not the host user
+RUNNER=$(command -v podman || command -v docker)
+
 ##########################################################
 # Runs Docker container to run build scripts from this dir
 ##########################################################
 dev-container () {
-  # port 3002 is used for developing Preferences UI
-
+  if [ -z "$RUNNER" ]; then
+      echo "You need podman or docker to run the container"
+      exit 1
+  fi
   # If SELinux is enabled, the volumes mounted into the container
   # need to have the right labels. This is accomplished by appending
   # ":z" to the volume option.
@@ -16,13 +22,14 @@ dev-container () {
       vol_suffix=""
   fi
 
-  exec docker run \
+  # port 3002 is used for developing Preferences UI
+  exec $RUNNER run \
     --rm \
     -it \
     -v $(pwd):/root/ulauncher${vol_suffix} \
     -v $HOME/.bash_history:/root/.bash_history${vol_suffix} \
     -p 3002:3002 \
     --name ulauncher \
-    $BUILD_IMAGE \
+    docker.io/$BUILD_IMAGE \
     bash
 }
