e4824dfb5c2db480eee65c1850c5a172ad6482af
Albin Larsson <mail@albinlarsson.com>
2022-07-12 21:28:14 +0200

    Rewrite extension finder to be faster and more terse

--- ulauncher/modes/extensions/extension_finder.py
+++ ulauncher/modes/extensions/extension_finder.py
@@ -5,11 +5,7 @@ def find_extensions(ext_dir):
     """
     Yields `(extension_id, extension_path)` tuples found in a given extensions dir
     """
-    if not os.path.exists(ext_dir):
-        return
-
-    dirs = [d for d in os.listdir(ext_dir) if os.path.isdir(os.path.join(ext_dir, d))]
-    for dir in dirs:
-        ext_path = os.path.join(ext_dir, dir)
-        if os.path.isfile(os.path.join(ext_path, 'manifest.json')):
-            yield (dir, ext_path)
+    if os.path.exists(ext_dir):
+        for entry in os.scandir(ext_dir):
+            if entry.is_dir() and os.path.isfile(f"{ext_dir}/{entry.name}/manifest.json"):
+                yield (entry.name, f"{ext_dir}/{entry.name}")
