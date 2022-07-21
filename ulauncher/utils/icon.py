6bf031893d6c20d5907b7b344be169de9677ad7d
Albin Larsson <mail@albinlarsson.com>
2022-07-21 13:45:17 +0200

    Avoid importing config in utils

--- ulauncher/utils/icon.py
+++ ulauncher/utils/icon.py
@@ -8,12 +8,12 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('GdkPixbuf', '2.0')
 # pylint: disable=wrong-import-position
 from gi.repository import Gtk, GdkPixbuf
-from ulauncher.config import PATHS
+from ulauncher import ASSETS
 
 icon_theme = Gtk.IconTheme.get_default()
 logger = logging.getLogger()
 
-DEFAULT_EXE_ICON = f"{PATHS.ASSETS}/icons/executable.png"
+DEFAULT_EXE_ICON = f"{ASSETS}/icons/executable.png"
 
 
 def get_icon_path(icon, size=32, base_path=""):
