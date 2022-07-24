94301d5c2a7a2e1a1d3b2ee24078c01b446fced2
Albin Larsson <mail@albinlarsson.com>
2022-07-24 21:09:49 +0200

    Add new method ExtensionManifest.find_matching_trigger

--- ulauncher/modes/extensions/ExtensionManifest.py
+++ ulauncher/modes/extensions/ExtensionManifest.py
@@ -155,6 +155,13 @@ class ExtensionManifest(JsonData):
             else:
                 raise ExtensionIncompatibleWarning(f'{self.name} does not support Ulauncher API v{API_VERSION}.')
 
+    def find_matching_trigger(self, **kwargs) -> Optional[str]:
+        """
+        Get the first trigger matching the arguments, and returns the id
+        Ex find_matching_trigger(user_keyword='asdf', icon=None)
+        """
+        return next((id for id, t in self.triggers.items() if {**t, **kwargs} == t), None)
+
     def get_user_preferences(self) -> Dict[str, Any]:
         """
         Get the preferences as an id-value dict
