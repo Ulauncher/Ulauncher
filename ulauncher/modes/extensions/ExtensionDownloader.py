30f2a9d675767257887bb2d298b79eaf30193567
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 06:21:57 +0000

    Adding tarfile member sanitization to extractall()

--- ulauncher/modes/extensions/ExtensionDownloader.py
+++ ulauncher/modes/extensions/ExtensionDownloader.py
@@ -136,7 +136,26 @@ def untar(filename: str, ext_path: str) -> None:
     temp_ext_path = mkdtemp(prefix='ulauncher_dl_')
 
     with tarfile.open(filename, mode="r") as archive:
-        archive.extractall(temp_ext_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(archive, temp_ext_path)
 
     for dir in os.listdir(temp_ext_path):
         move(os.path.join(temp_ext_path, dir), ext_path)
