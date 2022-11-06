a2e0aeb0966f2ba62ec18806b4498df5ebfef366
Albin Larsson <mail@albinlarsson.com>
2022-11-06 12:55:22 +0100

    Improve untar function

--- ulauncher/utils/untar.py
+++ ulauncher/utils/untar.py
@@ -1,16 +1,26 @@
 import tarfile
-from os.path import exists
+import os
 from shutil import rmtree
+from pathlib import Path
+from typing import Union
+
+
+# This function is the same as Path.is_relative_to in 3.9, but we can't use that yet
+def is_relative_to(child_path: Union[str, os.PathLike], root_path: Union[str, os.PathLike]):
+    return Path(root_path).resolve() in Path(child_path).resolve().parents
 
 
 def untar(archive_path: str, output_path: str, overwrite=True, strip=0) -> None:
-    if overwrite and exists(output_path):
+    if overwrite and os.path.exists(output_path):
         rmtree(output_path)
 
     with tarfile.open(archive_path, mode="r") as archive:
         for member in archive.getmembers():
-            if member.path.startswith("/") or member.path.startswith("../"):
-                raise Exception("Attempted Path Traversal in tar file")
+            # Tarfiles allow file names starting with "/", or containing "../" etc
+            # See https://github.com/advisories/GHSA-gw9q-c7gh-j9vm
+            if not is_relative_to(Path(output_path, member.name), output_path):
+                # Normalise the path to just the basename
+                strip = -1
 
             # Change member paths to strip N levels, like untar --strip-components=N
             member.path = member.path.split('/', strip)[-1]
