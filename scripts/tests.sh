285714ae2cffa3c7c87e58d0965b354b813ec0b0
Albin Larsson <mail@albinlarsson.com>
2023-01-22 16:28:18 +0100

    chore: simplify test command
    Always use xvfb-run (on display 99), if present. It will always work, at
    least on X11.
    Add -s argument to print stdout (otherwise debugging tests is a pain)

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -27,22 +27,13 @@ test-flake8 () {
 test-pytest () {
     set +e
     echo '[ test: pytest ]'
-    # Some tests will fail in Docker unless virtual frame buffer is running
-    if [ -f /.dockerenv -o -f /run/.containerenv ]; then
-        export DISPLAY=:1
-        ps cax | grep Xvfb > /dev/null
-        if [ $? -eq 0 ]; then
-            echo "Xvfb is already running"
-        else
-            echo "Start Xvfb on display $DISPLAY"
-            Xvfb :1 -screen 0 1024x768x16 &
-        fi
+    # Always use xvfb-run (on display 99) if present
+    if ! [ -x "$(command -v xvfb-run)" ]
+    then
+        DISPLAY=:99 xvfb-run pytest -s tests
+    else
+        pytest -s tests
     fi
-
-    # Not really needed with conftest.py in the root
-    export PYTHONPATH=`pwd`
-
     set -e
-    py.test $@ tests
 }
 
