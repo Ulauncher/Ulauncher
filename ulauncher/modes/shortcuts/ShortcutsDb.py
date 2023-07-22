528fc77b652861187540b3e28907aece687c496c
Albin Larsson <mail@fridaythe14th.com>
2023-07-22 14:10:10 +0200

    refactor: migrate ShortcutsDB to JsonConf

--- ulauncher/modes/shortcuts/ShortcutsDb.py
+++ ulauncher/modes/shortcuts/ShortcutsDb.py
@@ -3,32 +3,34 @@ from time import time
 
 from ulauncher.config import PATHS
 from ulauncher.utils.fold_user_path import fold_user_path
-from ulauncher.utils.json_data import JsonData, json_data_class
+from ulauncher.utils.json_conf import JsonConf
 
 
-@json_data_class
-class Shortcut(JsonData):
+class Shortcut(JsonConf):
     name = ""
     keyword = ""
     cmd = ""
     icon = ""
     is_default_search = True
     run_without_argument = False
-    added = 0.0
+    added = 0
     id = ""
 
 
-class ShortcutsDb(JsonData):
+class ShortcutsDb(JsonConf):
     # Coerce all values to Shortcuts instead of dict and fold the icon path
-    def __setitem__(self, key, value):
-        if hasattr(value, "icon"):
-            value.icon = fold_user_path(value.icon)
+    def __setitem__(self, key, value: dict):
+        if "added" in value and isinstance(value.get("added"), float):
+            # convert legacy float timestamps ulauncher used
+            value["added"] = int(value["added"])
+        if "icon" in value:
+            value["icon"] = fold_user_path(value["icon"])
         super().__setitem__(key, Shortcut(value))
 
     @classmethod
     def load(cls):
         file_path = Path(f"{PATHS.CONFIG}/shortcuts.json")
-        instance = cls.new_from_file(file_path)
+        instance = super().load(file_path)
         if not file_path.exists():
             added = int(time())
             keywords = [
@@ -57,6 +59,7 @@ class ShortcutsDb(JsonData):
                     icon=f"{PATHS.ASSETS}/icons/wikipedia.png",
                 ),
             ]
-            instance.save({keyword.id: keyword for keyword in keywords})
+            instance.update({keyword.id: keyword for keyword in keywords})
+            instance.save()
 
         return instance
