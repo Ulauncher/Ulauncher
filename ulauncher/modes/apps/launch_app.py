936b3b564aa61862c3226e814d3703c5ae54e36e
Albin Larsson <mail@fridaythe14th.com>
2023-07-29 13:22:01 +0200

    fix: fix bad assumption application ids with .desktop in the middle of the name

--- ulauncher/modes/apps/launch_app.py
+++ ulauncher/modes/apps/launch_app.py
@@ -1,7 +1,7 @@
 import logging
 import re
 import shlex
-from os.path import basename
+from pathlib import Path
 
 from gi.repository import Gio
 
@@ -13,12 +13,13 @@ from ulauncher.utils.wm import get_windows_stacked, get_xserver_time
 logger = logging.getLogger()
 
 
-def launch_app(app_id):
+def launch_app(desktop_entry_name):
+    app_id = Path(desktop_entry_name).stem if desktop_entry_name.endswith(".desktop") else desktop_entry_name
     settings = Settings.load()
-    app = Gio.DesktopAppInfo.new(app_id)
+    app = Gio.DesktopAppInfo.new(desktop_entry_name)
     # strip field codes %f, %F, %u, %U, etc
     app_exec = re.sub(r"\%[uUfFdDnNickvm]", "", app.get_commandline()).strip()
-    app_wm_id = (app.get_string("StartupWMClass") or basename(app_exec)).lower()
+    app_wm_id = (app.get_string("StartupWMClass") or Path(app_exec).name).lower()
     if app_exec and IS_X11 and (settings.raise_if_started or app.get_boolean("SingleMainWindow")):
         for win in get_windows_stacked():
             win_app_wm_id = (win.get_class_group_name() or "").lower()
@@ -32,7 +33,7 @@ def launch_app(app_id):
 
     if app.get_boolean("DBusActivatable"):
         # https://wiki.gnome.org/HowDoI/DBusApplicationLaunching
-        cmd = ["gapplication", "launch", app_id.replace(".desktop", "")]
+        cmd = ["gapplication", "launch", app_id]
     elif app_exec:
         if app.get_boolean("Terminal"):
             terminal_exec = settings.terminal_command
