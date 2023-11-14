b087470cc06c5591400513aceec9f6cbde7677ec
Albin Larsson <mail@fridaythe14th.com>
2023-11-14 21:59:02 +0100

    refactor: docs conf autor

--- docs/conf.py
+++ docs/conf.py
@@ -56,8 +56,8 @@ master_doc = "index"
 
 # General information about the project.
 project = "Ulauncher"
-copyright = f"2015-{datetime.now().year}, Aleksandr Gornostal"
 author = "Aleksandr Gornostal"
+copyright = f"2015-{datetime.now().year}, {author}"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -135,7 +135,7 @@ latex_elements: dict[str, str] = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, "Ulauncher.tex", "Ulauncher Documentation", "Aleksandr Gornostal", "manual"),
+    (master_doc, "Ulauncher.tex", "Ulauncher Documentation", author, "manual"),
 ]
 
 
