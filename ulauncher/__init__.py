1cdaeb4d28eacfddda887690072d4e3ecd02baff
Albin Larsson <mail@fridaythe14th.com>
2023-11-19 23:21:43 +0100

    wip: load version and gi_versions from toml (note: this is wip, we preferably don't want to do this as we just got rid of that build step in #1313)

--- ulauncher/__init__.py
+++ ulauncher/__init__.py
@@ -1,11 +1,19 @@
-version = "6.0.0-beta4"
-gi_versions = {
-    "Gtk": "3.0",
-    "Gdk": "3.0",
-    "GdkX11": "3.0",
-    "GdkPixbuf": "2.0",
-    "Pango": "1.0",
-}
+import sys
+from pathlib import Path
+
+if sys.version_info >= (3, 11):
+    import tomllib  # tomllib only exists in python>=3.11
+else:
+    import tomli as tomllib
+
+
+_project_root = Path(__file__).parent.parent
+
+with Path(_project_root, "pyproject.toml").open("rb") as f:
+    _pyproject = tomllib.load(f)
+    version = _pyproject["project"]["version"]
+    gi_versions = _pyproject.get("tool", {}).get("gobject", {}).get("pin_versions", {})
+
 
 # this namespace module is the only way we can pin gi versions globally,
 # but we also use it when we build, then we don't want to require gi
