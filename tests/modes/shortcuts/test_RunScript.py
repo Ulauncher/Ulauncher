590d881ea8e1ce1554e60d6bc6ce28f921ac816c
Krzysztof Nazarewski <gpg@kdn.im>
2023-11-21 12:44:22 +0100

    test_RunScript: make patchable without breaking black

--- tests/modes/shortcuts/test_RunScript.py
+++ tests/modes/shortcuts/test_RunScript.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 from random import randint
 
 from ulauncher.modes.shortcuts.run_script import run_script
@@ -10,7 +11,13 @@ class TestRunScriptAction:
         arg = "hello world"
         # RunScriptAction only supports bash argument placeholders ("$@" or "$1")
         # Shortcuts also support "%s", but that should be handled before they get here.
-        thread = run_script(f"#!/bin/bash\necho $@ > {test_file}", arg)
+        script = textwrap.dedent(
+            f"""\
+                #!/bin/bash
+                echo $@ > {test_file}
+            """
+        )
+        thread = run_script(script, arg)
         thread.join()
         with open(test_file) as f:
             assert f.read() == f"{arg}\n"
