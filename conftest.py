61c8346100ae35af182f942e2bb34681899f9787
Albin Larsson <mail@fridaythe14th.com>
2023-12-02 15:05:52 +0100

    chore: set mock xdg dirs for tests
    to make sure we never load or overwrite users conf/state when testing

--- conftest.py
+++ conftest.py
@@ -2,4 +2,16 @@
 
 import os
 
-os.environ["ULAUNCHER_SYSTEM_DATA_DIR"] = f"{os.path.dirname(__file__)}/data"
+# These shouldn't be written to, but 
+TEST_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "./.tmp/ulauncher_tests"))
+
+mock_xdg_dirs = {
+    "XDG_CONFIG_HOME": f"{TEST_ROOT}/.xdg-home-config",
+    "XDG_DATA_HOME": f"{TEST_ROOT}/.xdg-home-share",
+    "XDG_STATE_HOME": f"{TEST_ROOT}/.xdg-home-state",
+}
+
+for path in mock_xdg_dirs.values():
+    os.makedirs(path, exist_ok=True)
+
+os.environ.update(mock_xdg_dirs, ULAUNCHER_SYSTEM_DATA_DIR=f"{os.path.dirname(__file__)}/data")
