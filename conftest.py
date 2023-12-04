9086bc79af3e049166016e57a4c7d0e4fef72008
Albin Larsson <mail@fridaythe14th.com>
2023-12-04 13:05:15 +0100

    chore: override cli arguments that pytests passes to ulauncher

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 # This file has to exist for `pytest` to find the ulauncher module
 
 import os
+import sys
 
 # These paths shouldn't be written to, but if something goes wrong at least we won't overwrite user confs
 TEST_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "./.tmp/ulauncher_tests"))
@@ -15,3 +16,8 @@ for path in mock_xdg_dirs.values():
     os.makedirs(path, exist_ok=True)
 
 os.environ.update(mock_xdg_dirs, ULAUNCHER_SYSTEM_DATA_DIR=f"{os.path.dirname(__file__)}/data")
+
+# prevent leaking pytest arguments to ulaunchers arg parser
+# this way is not recommended, but it works and avoids needing tons of fixtures
+# https://stackoverflow.com/q/18668947/633921
+sys.argv = [sys.argv[0]]
