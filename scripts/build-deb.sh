bb57046ef1789e4f64a6171064b82100fb309a57
Albin Larsson <mail@fridaythe14th.com>
2024-01-18 20:26:09 +0100

    build(deb): split up into build-deb and release-deb and simplify

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -1,33 +1,18 @@
 #!/usr/bin/env bash
 
-######################
-# Builds Ulauncher.deb
-######################
-build-deb () {
-    # Args:
-    #   $1 --upload (optional)
-    #
-    # Env vars:
-    #   PPA required for upload
-    #   RELEASE release name required for upload
-    #   GPGKEY [optional] for upload
-
-    set -e
-
-    version=$(./ul version)
+prep-deb () {
+    VERSION=$(./ul version)
     # Debian prerelease separator is "~" instead of "-" (semver prerelease separator)
-    deb_version=$(echo "$version" | tr "-" "~")
+    DEB_VERSION=$(echo "$VERSION" | tr "-" "~")
+    TMPDIR="/tmp/ulauncher"
 
-    h2 "Building DEB package for Ulauncher $version..."
+    echo "Building DEB package for Ulauncher $VERSION..."
 
-    src_dir=$(pwd)
-    tmpdir="/tmp/ulauncher"
-
-    info "Building Preferences UI"
+    echo "Building Preferences UI"
     ./setup.py build_prefs --force
 
-    info "Copying src to a temp dir"
-    rm -rf $tmpdir/* || true
+    echo "Copying src to a temp dir"
+    rm -rf "${TMPDIR:?}/"* || true
     rsync -aq --progress \
         AUTHORS \
         bin \
@@ -41,51 +26,51 @@ build-deb () {
         io.ulauncher.Ulauncher.desktop \
         io.ulauncher.Ulauncher.service \
         ulauncher.service \
-        $tmpdir \
+        $TMPDIR \
         --exclude-from=.gitignore
 
     # This is only needed because data/preferences is in .gitignore
-    cp -r data/preferences $tmpdir/data/preferences
+    cp -r data/preferences $TMPDIR/data/preferences
 
-    cd $tmpdir
+    cd $TMPDIR || exit
+}
 
-    if [ "$1" = "--upload" ]; then
-        if [ -z "$RELEASE" ]; then
-            error "RELEASE env var is not supplied"
-            exit 1
-        fi
-        if [ -z "$PPA" ]; then
-            error "PPA env var is not supplied"
-            exit 1
-        fi
+# Env vars:
+#   GPGKEY [optional] for signing
+build-deb () {
+    prep-deb
+    dch --create --no-multimaint --package ulauncher --newversion="$DEB_VERSION" --empty --distribution focal
+    if [ -z "$GPGKEY" ]; then
+        echo "Building signed deb package"
+        dpkg-buildpackage -tc -us -sa "-k$GPGKEY"
+    else
+        echo "Building unsigned deb package"
+        dpkg-buildpackage -tc --no-sign
+    fi
+    rm debian/changelog || true
+    success "ulauncher_${DEB_VERSION}_all.deb saved to /tmp"
+}
 
-        info "Importing GPG keys"
-        if gpg --list-keys | grep -q $GPGKEY; then
-            info "GPG key is already imported"
-        else
-            gpg --import $src_dir/scripts/launchpad-public.key
-            gpg --import $src_dir/scripts/launchpad-secret.key
-            success "GPG key has been imported"
-        fi
+# Env vars:
+#   GPGKEY for signing
+#   PPA required for upload
+#   RELEASE release name required for upload
+release-deb () {
+    if [ -z "$PPA" ]; then
+        error "PPA env var is not supplied"
+        exit 1
+    fi
+    
+    prep-deb
 
-        info "Starting dpkg-buildpackage"
-        rm debian/changelog || true
-        dch --create --no-multimaint --package ulauncher --newversion=${deb_version}-0ubuntu1ppa1~$RELEASE --empty --distribution $RELEASE
-        dpkg-buildpackage -tc -S -sa -k$GPGKEY
+    for RELEASE in "$@"; do
+        info "Making release for $RELEASE"
+        dch --create --no-multimaint --package ulauncher --newversion="${DEB_VERSION}-0ubuntu1ppa1~$RELEASE" --empty --distribution "$RELEASE"
+        dpkg-buildpackage -tc -S -sa "-k$GPGKEY"
 
         info "Uploading to launchpad"
-        dput -f ppa:$PPA ../*.changes
-        rm /tmp/ulauncher_* || true
-    else
+        dput -f "ppa:$PPA" ../*.changes
         rm debian/changelog || true
-        dch --create --no-multimaint --package ulauncher --newversion=$deb_version --empty --distribution focal
-        if [ -z "$GPGKEY" ]; then
-            info "Building signed deb package"
-            dpkg-buildpackage -tc -us -sa -k$GPGKEY
-        else
-            info "Building unsigned deb package"
-            dpkg-buildpackage -tc --no-sign
-        fi
-        success "ulauncher_${deb_version}_all.deb saved to /tmp"
-    fi
+        rm /tmp/ulauncher_* || true
+    done
 }
