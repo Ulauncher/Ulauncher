bcc5f61c2b4c802135ed7ca37d36c7edcd78cf75
Albin Larsson <mail@fridaythe14th.com>
2024-02-25 00:14:57 +0100

    fix: restore setsid if not running ulauncher through systemd (#1358)
    * fix: restore setsid if not running ulauncher through systemd
    
    * chore: remove warning about systemd support

--- ulauncher/utils/launch_detached.py
+++ ulauncher/utils/launch_detached.py
@@ -1,25 +1,19 @@
 import logging
 import os
-from shutil import which
 
 from gi.repository import GLib
 
-logger = logging.getLogger()
-use_systemd_run = which("systemd-run") and os.system("systemd-run --user --scope true  2> /dev/null") == 0
+from ulauncher.utils.systemd_controller import SystemdController
 
-if not use_systemd_run:
-    logger.warning(
-        "Your system does not support launching applications in isolated scopes.\n"
-        "This may result in applications launched by Ulauncher to depend on the main\n"
-        "process (Ulauncher) and forced to exit prematurely if Ulauncher exits or crashes.\n"
-        "Most likely this is caused by using an outdated or or misconfigured system.\n\n"
-        "For more details see https://github.com/Ulauncher/Ulauncher/discussions/1352"
-    )
+logger = logging.getLogger()
 
 
 def launch_detached(cmd):
-    if use_systemd_run:
-        cmd = ["systemd-run", "--user", "--scope", *cmd]
+    cmd = (
+        ["systemd-run", "--user", "--scope", *cmd]
+        if SystemdController("ulauncher").is_active()
+        else ["setsid", "nohup", *cmd]
+    )
 
     env = dict(os.environ.items())
     # Make sure GDK apps aren't forced to use x11 on wayland due to ulauncher's need to run
@@ -33,7 +27,8 @@ def launch_detached(cmd):
             argv=cmd,
             envp=envp,
             flags=GLib.SpawnFlags.SEARCH_PATH_FROM_ENVP | GLib.SpawnFlags.SEARCH_PATH,
-            child_setup=None if use_systemd_run else os.setsid,
+            standard_output=True,
+            standard_error=True,
         )
     except Exception:
         logger.exception('Could not launch "%s"', cmd)
