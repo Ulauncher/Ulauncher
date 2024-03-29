29fb0a38d75d56724055eb75b501f6054082b480
Albin Larsson <mail@fridaythe14th.com>
2024-03-29 23:24:31 +0100

    refactor: move config paths to dedicated module

--- ulauncher/config.py
+++ ulauncher/config.py
@@ -2,52 +2,15 @@ from __future__ import annotations
 
 import argparse
 import os
-import sys
 from functools import lru_cache
 from gettext import gettext
 
 import ulauncher
+from ulauncher import paths as PATHS
 
 APP_ID = "io.ulauncher.Ulauncher"
 API_VERSION = "3.0"
-# spec: https://specifications.freedesktop.org/menu-spec/latest/ar01s02.html
-APPLICATION = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-# ULAUNCHER_SYSTEM_PREFIX is used by a third party packagers like Nix
-SYSTEM_PREFIX = os.environ.get("ULAUNCHER_SYSTEM_PREFIX", sys.prefix)
-# ULAUNCHER_SYSTEM_DATA_DIR is used when running in dev mode from source and during tests
-SYSTEM_DATA_DIR = os.path.abspath(os.environ.get("ULAUNCHER_SYSTEM_DATA_DIR", f"{SYSTEM_PREFIX}/share/ulauncher"))
-HOME = os.path.expanduser("~")
-XDG_DATA_DIRS = os.environ.get("XDG_DATA_DIRS", f"/usr/local/share/{os.path.pathsep}/usr/share/").split(os.path.pathsep)
-USER_CONFIG_DIR = os.path.join(os.environ.get("XDG_CONFIG_HOME", f"{HOME}/.config"), "ulauncher")
-USER_DATA_DIR = os.path.join(os.environ.get("XDG_DATA_HOME", f"{HOME}/.local/share"), "ulauncher")
-USER_STATE_DIR = os.path.join(os.environ.get("XDG_STATE_HOME", f"{HOME}/.local/state"), "ulauncher")
-USER_EXTENSIONS_DIR = os.path.join(USER_DATA_DIR, "extensions")
-ALL_EXTENSIONS_DIRS = [USER_EXTENSIONS_DIR, *[os.path.join(p, "ulauncher", "extensions") for p in XDG_DATA_DIRS]]
-EXTENSIONS_CONFIG_DIR = os.path.join(USER_CONFIG_DIR, "ext_preferences")
-EXTENSIONS_STATE_DIR = os.path.join(USER_STATE_DIR, "ext_state")
-USER_THEMES = os.path.join(USER_CONFIG_DIR, "user-themes")
-SYSTEM_THEMES = os.path.join(SYSTEM_DATA_DIR, "themes")
 VERSION = ulauncher.version
-
-
-# Would use SimpleNamespace if that worked with typing and auto-completion.
-class _PATHS_CLASS:
-    APPLICATION = APPLICATION
-    ASSETS = SYSTEM_DATA_DIR
-    HOME = HOME
-    CONFIG = USER_CONFIG_DIR
-    DATA = USER_DATA_DIR
-    STATE = USER_STATE_DIR
-    USER_EXTENSIONS = USER_EXTENSIONS_DIR
-    ALL_EXTENSIONS_DIRS = ALL_EXTENSIONS_DIRS
-    EXTENSIONS_CONFIG = EXTENSIONS_CONFIG_DIR
-    EXTENSIONS_STATE = EXTENSIONS_STATE_DIR
-    USER_THEMES = USER_THEMES
-    SYSTEM_THEMES = SYSTEM_THEMES
-
-
-PATHS = _PATHS_CLASS()
-
 FIRST_RUN = not os.path.exists(PATHS.CONFIG)  # If there is no config dir, assume it's the first run
 FIRST_V6_RUN = not os.path.exists(PATHS.STATE)
 
---
+++ ulauncher/paths.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+# spec: https://specifications.freedesktop.org/menu-spec/latest/ar01s02.html
+APPLICATION = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# ULAUNCHER_SYSTEM_PREFIX is used by a third party packagers like Nix
+SYSTEM_PREFIX = os.environ.get("ULAUNCHER_SYSTEM_PREFIX", sys.prefix)
+# ULAUNCHER_SYSTEM_DATA_DIR is used when running in dev mode from source and during tests
+ASSETS = os.path.abspath(os.environ.get("ULAUNCHER_SYSTEM_DATA_DIR", f"{SYSTEM_PREFIX}/share/ulauncher"))
+HOME = os.path.expanduser("~")
+XDG_DATA_DIRS = os.environ.get("XDG_DATA_DIRS", f"/usr/local/share/{os.path.pathsep}/usr/share/").split(os.path.pathsep)
+CONFIG = os.path.join(os.environ.get("XDG_CONFIG_HOME", f"{HOME}/.config"), "ulauncher")
+DATA = os.path.join(os.environ.get("XDG_DATA_HOME", f"{HOME}/.local/share"), "ulauncher")
+STATE = os.path.join(os.environ.get("XDG_STATE_HOME", f"{HOME}/.local/state"), "ulauncher")
+USER_EXTENSIONS = os.path.join(DATA, "extensions")
+ALL_EXTENSIONS_DIRS = [USER_EXTENSIONS, *[os.path.join(p, "ulauncher", "extensions") for p in XDG_DATA_DIRS]]
+EXTENSIONS_CONFIG = os.path.join(CONFIG, "ext_preferences")
+EXTENSIONS_STATE = os.path.join(STATE, "ext_state")
+USER_THEMES = os.path.join(CONFIG, "user-themes")
+SYSTEM_THEMES = os.path.join(ASSETS, "themes")
