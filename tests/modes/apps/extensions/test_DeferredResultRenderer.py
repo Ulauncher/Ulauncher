f02b6d1d0e3d5ba8574c53b4faa58e05910002b4
Albin Larsson <mail@fridaythe14th.com>
2024-04-08 18:18:12 +0200

    test(DeferredResultRenderer): remove unused tests

--- tests/modes/apps/extensions/test_DeferredResultRenderer.py
+++ tests/modes/apps/extensions/test_DeferredResultRenderer.py
@@ -2,11 +2,7 @@ from unittest import mock
 
 import pytest
 
-from ulauncher.api.shared.event import BaseEvent, KeywordQueryEvent
-from ulauncher.api.shared.query import Query
 from ulauncher.modes.extensions.DeferredResultRenderer import DeferredResultRenderer
-from ulauncher.modes.extensions.ExtensionController import ExtensionController
-from ulauncher.modes.extensions.ExtensionManifest import ExtensionManifest
 from ulauncher.ui.UlauncherApp import UlauncherApp
 
 app = UlauncherApp()
@@ -18,42 +14,9 @@ class TestDeferredResultRenderer:
         app.window = mocker.patch("ulauncher.ui.windows.UlauncherWindow.UlauncherWindow").return_value
         return app.window
 
-    @pytest.fixture(autouse=True)
-    def timer(self, mocker):
-        return mocker.patch("ulauncher.modes.extensions.DeferredResultRenderer.timer")
-
-    @pytest.fixture
-    def event(self):
-        return mock.create_autospec(BaseEvent)
-
-    @pytest.fixture
-    def manifest(self):
-        return mock.create_autospec(ExtensionManifest)
-
-    @pytest.fixture
-    def controller(self):
-        return mock.create_autospec(ExtensionController)
-
     @pytest.fixture
     def renderer(self):
         return DeferredResultRenderer()
-
-    def test_handle_response__action__is_ran(self, renderer, controller):
-        action = mock.Mock()
-        event = KeywordQueryEvent(Query("test"))
-        response = {"event": event, "action": action}
-        renderer.active_event = event
-        renderer.active_controller = controller
-        renderer.handle_response(response, controller)
-
-    def test_handle_response__keep_app_open_is_False__hide_is_called(self, renderer, controller):
-        action = mock.Mock()
-        action.keep_app_open = False
-        event = KeywordQueryEvent(Query("test"))
-        response = {"event": event, "action": action}
-        renderer.active_event = event
-        renderer.active_controller = controller
-        renderer.handle_response(response, controller)
 
     def test_on_query_change__loading__is_canceled(self, renderer):
         timer = mock.Mock()
