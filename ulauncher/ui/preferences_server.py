5de4f443f3195d5dce2baf57eec2fa2a286ab3b5
Albin Larsson <mail@fridaythe14th.com>
2024-04-14 13:39:42 +0200

    refactor: preferences server doesn't have to be a singleton (it has state and is only accessed from one place)

--- ulauncher/ui/preferences_server.py
+++ ulauncher/ui/preferences_server.py
@@ -21,7 +21,6 @@ from ulauncher.utils.environment import IS_X11
 from ulauncher.utils.hotkey_controller import HotkeyController
 from ulauncher.utils.launch_detached import open_detached
 from ulauncher.utils.Settings import Settings
-from ulauncher.utils.singleton import Singleton
 from ulauncher.utils.systemd_controller import SystemdController
 from ulauncher.utils.Theme import get_themes
 from ulauncher.utils.WebKit2 import WebKit2
@@ -57,7 +56,7 @@ def get_extensions() -> Generator[dict[str, Any], None, None]:
         }
 
 
-class PreferencesServer(metaclass=Singleton):
+class PreferencesServer:
     """
     Handles the "back-end" of the PreferencesWindow's wekit webview
     Because of how the WebKitGtk API is implemented you should never create more than one context for the same mainloop
