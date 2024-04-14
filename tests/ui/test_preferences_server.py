98c125161b2b2e275b629124bac6869cf2a6f53f
Albin Larsson <mail@fridaythe14th.com>
2024-04-14 20:39:01 +0200

    test: fix and simplify prefs server test using cached settings instance instead of file
    We set different paths for testing now (as of
    61c8346100ae35af182f942e2bb34681899f9787) so this is now safe

--- tests/ui/test_preferences_server.py
+++ tests/ui/test_preferences_server.py
@@ -1,7 +1,3 @@
-import json
-import shutil
-from pathlib import Path
-
 import pytest
 
 from ulauncher.ui.preferences_server import PreferencesServer
@@ -9,32 +5,14 @@ from ulauncher.ui.UlauncherApp import UlauncherApp
 from ulauncher.utils.Settings import Settings
 
 app = UlauncherApp()
-settings_file = "/tmp/ulauncher-test/pref-settings.json"
-
-
-def load_json():
-    try:
-        with open(settings_file) as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
+settings = Settings.load()
 
 
 class TestPreferencesServer:
-    def setup_class(self):
-        Path("/tmp/ulauncher-test").mkdir(parents=True, exist_ok=True)
-
-    def teardown_class(self):
-        shutil.rmtree("/tmp/ulauncher-test")
-
     @pytest.fixture(autouse=True)
     def ulauncherWindow(self, mocker):
         app.window = mocker.patch("ulauncher.ui.windows.UlauncherWindow.UlauncherWindow").return_value
         return app.window
-
-    @pytest.fixture(autouse=True)
-    def settings_file(self, mocker):
-        return mocker.patch("ulauncher.utils.Settings._settings_file", new=settings_file)
 
     @pytest.fixture(autouse=True)
     def autostart_pref(self, mocker):
@@ -55,7 +33,7 @@ class TestPreferencesServer:
     def test_apply_settings_show_tray_icon(self, prefs_server):
         prefs_server.apply_settings("show_tray_icon", False)
         assert prefs_server.settings.show_tray_icon is False
-        assert load_json().get("show_tray_icon") is False
+        assert settings.show_tray_icon is False
 
     def test_set_autostart(self, prefs_server, autostart_pref):
         prefs_server.apply_autostart(True)
@@ -65,6 +43,6 @@ class TestPreferencesServer:
         autostart_pref.toggle.assert_called_with(False)
 
     def test_set_grab_mouse_pointer_dash_underscore_conversion(self, prefs_server):
-        prefs_server.apply_settings("grab-mouse-pointer", True)
         # Verify that setting with dash character is converted to underscore
-        assert load_json().get("grab_mouse_pointer") is True
+        prefs_server.apply_settings("grab-mouse-pointer", True)
+        assert settings.grab_mouse_pointer is True
