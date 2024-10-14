039e586b0e07b8aca4a291cd7335af4e075ea606
Albin Larsson <515120+friday@users.noreply.github.com>
2024-10-14 19:43:25 +0200

    chore: update ubuntu upload script to replace mantic with oracular

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -97,8 +97,8 @@ launchpad_upload() {
 
     noble="PPA=$PPA RELEASE=noble ./ul build-deb $VERSION --upload"
     jammy="PPA=$PPA RELEASE=jammy ./ul build-deb $VERSION --upload"
+    oracular="PPA=$PPA RELEASE=oracular ./ul build-deb $VERSION --upload"
     focal="PPA=$PPA RELEASE=focal ./ul build-deb $VERSION --upload"
-    mantic="PPA=$PPA RELEASE=mantic ./ul build-deb $VERSION --upload"
 
     # extracts ~/.shh for uploading package to ppa.launchpad.net via sftp
     # then uploads each release
@@ -108,6 +108,6 @@ launchpad_upload() {
         --rm \
         -v $(pwd):/root/ulauncher \
         $BUILD_IMAGE \
-        bash -c "tar -xvf scripts/launchpad.ssh.tar -C / && $noble && $jammy && $focal && $mantic"
+        bash -c "tar -xvf scripts/launchpad.ssh.tar -C / && $noble && $jammy && $oracular && $focal"
     set +x
 }
