57d5ee641a45e657835cdebf531785ab6c83cdc1
James Aaron Erang <iaacornus.devel@gmail.com>
2024-12-22 02:04:10 +0800

    add a warning comment in the beginning of module

---
+++ ulauncher/modes/extensions/extension_environment.py
@@ -0,0 +1,5 @@
+"""
+!EXPERIMENTAL: this module intends to provide an implementation of automatic
+!installation and virtual environment management/setup automation of ulauncher
+!extensions installed using one way or another.
+"""
