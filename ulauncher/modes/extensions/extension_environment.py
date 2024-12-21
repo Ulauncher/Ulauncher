bc3daaa5fdec40a5463dc766c00eae7e61ee3c91
James Aaron Erang <iaacornus.devel@gmail.com>
2024-12-22 02:30:50 +0800

    PROTOTYPE: idea of feature implementation
    THIS IS A PROTOTYPE; refer to the comment 273#issuecomment-2553896283 for
    further details of the implementation, but to recall:
    
    1) create a venv for each extension, if it has a requirements.txt
    2) so we record the requirements.txt that extension and the name of env created for it (perhaps a hashmap?)
    3) should there be another extension that uses the same deps/version of dep, install that extension in the same env; otherwise, go back to step 1
    4) if it doesnt have requirements.txt, then just create a venv solely for it.

--- ulauncher/modes/extensions/extension_environment.py
+++ ulauncher/modes/extensions/extension_environment.py
@@ -8,17 +8,51 @@ from pathlib import Path
 from os.path import exists
 
 
-def check_requirements(ext_dir: Path) -> bool:
-    """check_requirements
+class ExtensionEnvironment:
+    def __init__(self, ext_dir: Path, venv_map: dict[str, set]) -> None:
+        self.ext_dir: Path = ext_dir
+        self.venv_map: dict[str, set[str]] = {
+            "test": set(
+                    [
+                        "hello==1.0"
+                    ]
+                )
+        }
 
-    Args:
-        ext_dir (Path): Path of the extension dir downloaded/cloned.
+    def check_requirements(self) -> bool:
+        """check_requirements
 
-    Returns:
-        bool: whether a requirements.txt file exists for the extension.
-    """
+        Args:
+            ext_dir (Path): Path of the extension dir downloaded/cloned.
 
-    if exists(ext_dir / "requirements.txt"):
-        return True # it should be requirements.txt only, not requirement.txt
-    return False
+        Returns:
+            bool: whether a requirements.txt file exists for the extension.
+        """
+
+        if exists(self.ext_dir / "requirements.txt"):
+            return True # it should be requirements.txt only, not requirement.txt
+        return False
+
+    def prep_extension(self):
+        req_path: Path = self.ext_dir / "requirements.txt"
+
+        if not self.check_requirements():
+            self.create_sole_venv()
+
+        with open(req_path, "r", encoding="utf-8") as req_file:
+            dependencies: set[str] = set(req_file.readlines())
+
+        for venv in self.venv_map.keys():
+            if dependencies >= self.venv_map.get(venv): # type: ignore
+                self.create_sole_venv()
+            else:
+                continue
+
+            self.install_ext(venv)
+
+    def create_sole_venv(self):
+        pass
+
+    def install_ext(self):
+        pass
 
