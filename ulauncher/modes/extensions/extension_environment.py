0510d1947c1dbc29286dedb9c15ffb0a00105751
James Aaron Erang <iaacornus.devel@gmail.com>
2024-12-22 02:15:59 +0800

    check whether requirements.txt exists for an extension

--- ulauncher/modes/extensions/extension_environment.py
+++ ulauncher/modes/extensions/extension_environment.py
@@ -3,3 +3,22 @@
 !installation and virtual environment management/setup automation of ulauncher
 !extensions installed using one way or another.
 """
+
+from pathlib import Path
+from os.path import exists
+
+
+def check_requirements(ext_dir: Path) -> bool:
+    """check_requirements
+
+    Args:
+        ext_dir (Path): Path of the extension dir downloaded/cloned.
+
+    Returns:
+        bool: whether a requirements.txt file exists for the extension.
+    """
+
+    if exists(ext_dir / "requirements.txt"):
+        return True # it should be requirements.txt only, not requirement.txt
+    return False
+
