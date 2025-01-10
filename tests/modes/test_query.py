5301444cf0364a92fd7f146f112c0271df14be42
Albin Larsson <515120+friday@users.noreply.github.com>
2025-01-10 23:34:42 +0100

    test(query): remove useless getter tests
    They are 100% the same as the props

--- tests/modes/test_query.py
+++ tests/modes/test_query.py
@@ -11,13 +11,3 @@ class TestQuery:
         assert Query("kw arg text").argument == "arg text"
         assert not Query("kw").argument
         assert not Query("").argument
-
-    def test_get_keyword(self):
-        assert Query("kw arg").get_keyword() == "kw"
-        assert Query("kw").get_keyword() == "kw"
-        assert not Query("").get_keyword()
-
-    def test_get_argument(self):
-        assert Query("kw arg text").get_argument() == "arg text"
-        assert not Query("kw").get_argument()
-        assert not Query("").get_argument()
