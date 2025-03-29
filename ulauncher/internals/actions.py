4c85a7c9deb437c9bc51d824eb6355857fe70318
Albin Larsson <515120+friday@users.noreply.github.com>
2025-03-29 12:37:15 +0100

    refactor: add even more helpful details for invalid action data

--- ulauncher/internals/actions.py
+++ ulauncher/internals/actions.py
@@ -1,17 +1,44 @@
 from __future__ import annotations
 
+import logging
 from typing import Any
+
+logger = logging.getLogger()
 
 
 def copy(text: str) -> dict[str, str]:
+    if not text:
+        msg = "Copy argument cannot be empty"
+        logger.error(msg)
+        raise ValueError(msg)
+    if type(text) is not str:
+        msg = f'Copy argument "{text}" is invalid. It must be a string'
+        logger.error(msg)
+        raise TypeError(msg)
     return {"type": "action:clipboard_store", "data": text}
 
 
 def open(item: str) -> dict[str, str]:  # noqa: A001
+    if not item:
+        msg = "Open argument cannot be empty"
+        logger.error(msg)
+        raise ValueError(msg)
+    if type(item) is not str:
+        msg = f'Open argument "{item}" is invalid. It must be a string'
+        logger.error(msg)
+        raise TypeError(msg)
     return {"type": "action:open", "data": item}
 
 
 def run_script(script: str, args: str = "") -> dict[str, str | list[str]]:
+    if not script:
+        msg = "Script argument cannot be empty"
+        logger.error(msg)
+        raise ValueError(msg)
+    if type(script) is not str:
+        msg = f'Script argument "{script}" is invalid. It must be a string'
+        logger.error(msg)
+        raise TypeError(msg)
     return {"type": "action:legacy_run_script", "data": [script, args]}
 
 
