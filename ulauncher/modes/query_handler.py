Ulauncher/Ulauncher

commit f9481a0317e9df37fc71d32702d674209f63f230

refactor: rewrite Query class to allow more control from the modes

ulauncher/modes/query_handler.py
@@ -10,7 +10,7 @@ _events = EventBus("query")
 
 class QueryHandler:
     mode: BaseMode | None = None
-    query: Query = Query("")
+    query: Query = Query(None, "")
     triggers: list[Result] = []
 
     def __init__(self) -> None:
@@ -25,7 +25,7 @@ class QueryHandler:
 
     def parse(self, query_str: str) -> None:
         self.mode = None
-        self.query = Query(query_str)
+        self.query = Query(None, query_str)
 
         from ulauncher.modes.mode_handler import get_modes
 
@@ -61,9 +61,9 @@ class QueryHandler:
 
     def handle_backspace(self, query_str: str) -> bool:
         if self.mode:
-            new_query_str = self.mode.handle_backspace(query_str)
-            if new_query_str is not None:
-                self.parse(new_query_str)
-                _events.emit("app:set_query", new_query_str)
+            new_query = self.mode.handle_backspace(query_str)
+            if new_query is not None:
+                self.query = new_query
+                _events.emit("app:set_query", str(new_query))
                 return True
         return False

tests/modes/apps/extensions/test_extension_mode.py
@@ -16,24 +16,20 @@ class TestExtensionMode:
         controller = mock.create_autospec(ExtensionSocketController)
         ext_server.get_controller_by_keyword.return_value = controller
         mode = ExtensionMode()
-        query = Query("kw something")
-
-        assert mode.parse_query_str(query), "Mode is not enabled"
+        assert mode.parse_query_str("kw something"), "Mode is not enabled"
 
     def test_is_enabled__query_only_contains_keyword__returns_false(self, ext_server):
         controller = mock.create_autospec(ExtensionSocketController)
         ext_server.get_controller_by_keyword.return_value = controller
         mode = ExtensionMode()
-        query = Query("kw")
 
-        assert not mode.parse_query_str(query), "Mode is enabled"
+        assert not mode.parse_query_str("kw"), "Mode is enabled"
 
     def test_is_enabled__keyword__is_used_to_get_controller(self, ext_server):
         controller = mock.create_autospec(ExtensionSocketController)
         ext_server.get_controller_by_keyword.return_value = controller
         mode = ExtensionMode()
-        query = Query("kw something")
-        mode.parse_query_str(query)
+        mode.parse_query_str("kw something")
 
         ext_server.get_controller_by_keyword.assert_called_with("kw")
 
@@ -42,7 +38,7 @@ class TestExtensionMode:
         controller.ext_id = "com.example.asdf.ghjk"
         ext_server.get_controller_by_keyword.return_value = controller
         mode = ExtensionMode()
-        query = mock.create_autospec(Query)
+        query = Query("asdf", "ghjk")
         mode.handle_query(query)
 
         ext_server.get_controller_by_keyword.return_value.handle_query.assert_called_with(query)
@@ -52,5 +48,5 @@ class TestExtensionMode:
         controller.ext_id = "com.example.asdf.ghjk"
         ext_server.get_controller_by_keyword.return_value = controller
         mode = ExtensionMode()
-        query = mock.create_autospec(Query)
+        query = Query("asdf", "ghjk")
         assert mode.handle_query(query) is ext_server.get_controller_by_keyword.return_value.handle_query.return_value

tests/modes/apps/test_app_result.py
@@ -62,7 +62,7 @@ class TestAppResult:
 
     def test_on_activation(self, app1, mocker, app_starts):
         launch_app = mocker.patch("ulauncher.modes.apps.app_result.launch_app")
-        app1.on_activation(Query("query"))
+        app1.on_activation(Query(None, "query"))
         launch_app.assert_called_with("trueapp.desktop")
         assert app_starts.get("trueapp.desktop") == 766
 

tests/modes/calc/test_calc_mode.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 
 import pytest
 
+from ulauncher.internals.query import Query
 from ulauncher.modes.calc.calc_mode import CalcMode, eval_expr
 
 
@@ -68,17 +69,17 @@ class TestCalcMode:
         assert eval_expr("asinh(6) + atanh(0.9) + ln(0.7)") == "3.6073243982894"
 
     def test_handle_query(self, mode):
-        assert mode.handle_query("3+2")[0].result == "5"
-        assert mode.handle_query("3+2*")[0].result == "5"
-        assert mode.handle_query("2-2")[0].result == "0"
-        assert mode.handle_query("5%2")[0].result == "1"
+        assert mode.handle_query(Query(None, "3+2"))[0].result == "5"
+        assert mode.handle_query(Query(None, "3+2*"))[0].result == "5"
+        assert mode.handle_query(Query(None, "2-2"))[0].result == "0"
+        assert mode.handle_query(Query(None, "5%2"))[0].result == "1"
 
     def test_handle_query__invalid_expr(self, mode):
-        [invalid_result] = mode.handle_query("3++")
+        [invalid_result] = mode.handle_query(Query(None, "3++"))
         assert invalid_result.name == "Error!"
         assert invalid_result.description == "Invalid expression"
-        assert mode.handle_query("6 2")[0].name == "Error!"
-        assert mode.handle_query("()*2")[0].name == "Error!"
-        assert mode.handle_query("a+b")[0].name == "Error!"
-        assert mode.handle_query("sqrt()+1")[0].name == "Error!"
-        assert mode.handle_query("2pi")[0].name == "Error!"
+        assert mode.handle_query(Query(None, "6 2"))[0].name == "Error!"
+        assert mode.handle_query(Query(None, "()*2"))[0].name == "Error!"
+        assert mode.handle_query(Query(None, "a+b"))[0].name == "Error!"
+        assert mode.handle_query(Query(None, "sqrt()+1"))[0].name == "Error!"
+        assert mode.handle_query(Query(None, "2pi"))[0].name == "Error!"

tests/modes/file_browser/test_file_browser_mode.py
@@ -2,6 +2,7 @@ from types import SimpleNamespace
 
 import pytest
 
+from ulauncher.internals.query import Query
 from ulauncher.modes.file_browser.file_browser_mode import FileBrowserMode
 
 
@@ -57,8 +58,10 @@ class TestFileBrowserMode:
         assert mode.filter_dot_files(["a", ".b", "c", ".d"]) == ["a", "c"]
 
     def test_handle_query__path_from_q_exists__dir_listing_rendered(self):
-        flattened_results = [str(r.path) for r in FileBrowserMode().handle_query("/tmp/")]
+        query = Query(None, "/tmp/")
+        flattened_results = [str(r.path) for r in FileBrowserMode().handle_query(query)]
         assert flattened_results == ["/tmp/B", "/tmp/D", "/tmp/c", "/tmp/a"]
 
     def test_handle_query__invalid_path__empty_list_rendered(self, mode):
-        assert mode.handle_query("~~") == []
+        query = Query(None, "~~")
+        assert mode.handle_query(query) == []

tests/modes/shortcuts/test_shortcut_mode.py
@@ -47,14 +47,14 @@ class TestShortcutMode:
         assert mode.parse_query_str(query)
 
     def test_handle_query__return_value__is(self, mode, shortcuts_db, shortcut_result):
-        query = Query("kw something")
+        query = Query("kw", "something")
         shortcut = ShortcutRecord(keyword="kw")
         shortcuts_db.values.return_value = [shortcut]
 
         assert mode.handle_query(query)[0] == shortcut_result.return_value
 
     def test_handle_query__shortcut_result__is_called(self, mode, shortcuts_db, shortcut_result):
-        query = Query("kw something")
+        query = Query("kw", "something")
         shortcut = ShortcutRecord(keyword="kw")
         shortcuts_db.values.return_value = [shortcut]
 

tests/modes/shortcuts/test_shortcut_result.py
@@ -23,25 +23,25 @@ class TestShortcutResult:
         assert isinstance(result.icon, str)
 
     def test_on_activation(self, result, run_shortcut):
-        result = result.on_activation(Query("kw test"))
+        result = result.on_activation(Query("kw", "test"))
         run_shortcut.assert_called_once_with("https://site/?q=%s", "test")
         assert not isinstance(result, str)
 
     def test_on_activation__default_search(self, result, run_shortcut):
         result.is_default_search = True
-        result = result.on_activation(Query("search query"))
+        result = result.on_activation(Query(None, "search query"))
         run_shortcut.assert_called_once_with("https://site/?q=%s", "search query")
         assert not isinstance(result, str)
 
     def test_on_activation__run_without_arguments(self, result, run_shortcut):
         result.run_without_argument = True
-        result = result.on_activation(Query("kw"))
+        result = result.on_activation(Query("kw", None))
         # it doesn't replace %s if run_without_argument = True
         run_shortcut.assert_called_once_with("https://site/?q=%s", None)
         assert not isinstance(result, str)
 
     def test_on_activation__run_file(self, run_shortcut):
         result = ShortcutResult(keyword="kw", name="name", cmd="/usr/bin/something/%s", icon="icon_path")
-        result.on_activation(Query("kw query"))
+        result.on_activation(Query("kw", "query"))
         # Scripts should support both %s and arguments
         run_shortcut.assert_called_once_with("/usr/bin/something/%s", "query")

tests/modes/test_query.py
@@ -3,11 +3,20 @@ from ulauncher.internals.query import Query
 
 class TestQuery:
     def test_keyword(self):
-        assert Query("kw arg").keyword == "kw"
-        assert Query("kw").keyword == "kw"
-        assert not Query("").keyword
+        assert Query.parse_str("kw").keyword == "kw"
+        assert Query.parse_str("kw arg").keyword == "kw"
+        assert Query("kw", "arg").keyword == "kw"
+        assert Query("kw", None).keyword == "kw"
+        assert not Query(None, None).keyword
 
     def test_argument(self):
-        assert Query("kw arg text").argument == "arg text"
-        assert not Query("kw").argument
-        assert not Query("").argument
+        assert Query.parse_str("kw arg").argument == "arg"
+        assert Query("kw", "arg").argument == "arg"
+        assert not Query.parse_str("kw").argument
+        assert not Query("kw", None).argument
+        assert not Query(None, None).argument
+
+    def test_is_active(self):
+        assert Query.parse_str("kw arg").is_active
+        assert Query.parse_str("kw ").is_active
+        assert not Query.parse_str("kw").is_active

commit 08384fbb1c8e78bf4894895d42263ff222222440

fix: fallback shortcuts should not show on empty query
It wasn't shown initially, but when deleting the previous input and
having no "frequent apps".

Issue introduced in f9481a03 when rewriting Query to not be a string
subclass

ulauncher/modes/query_handler.py
@@ -64,7 +64,7 @@ class QueryHandler:
         # No mode selected, which means search
         results = self.search_triggers()
         # If the search result is empty, add the default items for all other modes (only shortcuts currently)
-        if not results and self.query:
+        if not results and str(self.query):
             for mode in get_modes():
                 res = mode.get_fallback_results()
                 results.extend(res)