55807ef0fe5b9e1a95c3f22e6a8d547e5c8117ba
Albin Larsson <515120+friday@users.noreply.github.com>
2025-07-22 22:31:59 +0200

    refactor(item-nav): remove now unused EventBus instance

--- ulauncher/ui/item_navigation.py
+++ ulauncher/ui/item_navigation.py
@@ -3,10 +3,8 @@ from __future__ import annotations
 from ulauncher import paths
 from ulauncher.modes.query_handler import QueryHandler
 from ulauncher.ui.result_widget import ResultWidget
-from ulauncher.utils.eventbus import EventBus
 from ulauncher.utils.json_utils import json_load, json_save
 
-events = EventBus()
 query_history_path = f"{paths.STATE}/query_history.json"
 query_history: dict[str, str] = json_load(query_history_path)
 
