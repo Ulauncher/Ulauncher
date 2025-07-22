a0afcf84f7aedfbe0ff006cd41d781fe6aeba769
Albin Larsson <515120+friday@users.noreply.github.com>
2025-07-22 21:50:40 +0200

    refactor: reorder namespace props

--- ulauncher/__init__.py
+++ ulauncher/__init__.py
@@ -4,8 +4,11 @@ import os
 
 from . import paths
 
+app_id = "io.ulauncher.Ulauncher"
 version = "6.0.0-beta20"
 api_version = "3.0"
+first_run = not os.path.exists(paths.CONFIG)  # If there is no config dir, assume it's the first run
+first_v6_run = not os.path.exists(paths.STATE)
 gi_versions = {
     "Gtk": "3.0",
     "Gdk": "3.0",
@@ -13,9 +16,6 @@ gi_versions = {
     "GdkPixbuf": "2.0",
     "Pango": "1.0",
 }
-app_id = "io.ulauncher.Ulauncher"
-first_run = not os.path.exists(paths.CONFIG)  # If there is no config dir, assume it's the first run
-first_v6_run = not os.path.exists(paths.STATE)
 
 # this namespace module is the only way we can pin gi versions globally,
 # but we also use it when we build, then we don't want to require gi
