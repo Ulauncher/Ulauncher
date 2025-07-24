3fbffceafd6351635931c7a6252f69323f63b66f
Albin Larsson <515120+friday@users.noreply.github.com>
2025-07-24 21:05:19 +0200

    refactor(core): clarify public/private properties

--- ulauncher/core.py
+++ ulauncher/core.py
@@ -34,22 +34,22 @@ def get_modes() -> list[BaseMode]:
 class UlauncherCore:
     """Core application logic to handle the query events and delegate them to the modes."""
 
-    mode: BaseMode | None = None
+    _mode: BaseMode | None = None
+    _triggers: list[Result] = []
+    _mode_map: WeakKeyDictionary[Result, BaseMode] = WeakKeyDictionary()
+    _triggers_loaded: bool = False
     query: Query = Query(None, "")
-    triggers: list[Result] = []
-    mode_map: WeakKeyDictionary[Result, BaseMode] = WeakKeyDictionary()
-    triggers_loaded: bool = False
 
     def load_triggers(self, force: bool = False) -> None:
-        if self.triggers_loaded and not force:
+        if self._triggers_loaded and not force:
             return
 
-        self.triggers.clear()
+        self._triggers.clear()
         for mode in get_modes():
             for trigger in mode.get_triggers():
-                self.triggers.append(trigger)
-                self.mode_map[trigger] = mode
-        self.triggers_loaded = True
+                self._triggers.append(trigger)
+                self._mode_map[trigger] = mode
+        self._triggers_loaded = True
 
     def update(self, query_str: str) -> None:
         """Parse the query string and update the mode and query."""
@@ -58,12 +58,12 @@ class UlauncherCore:
             # otherwise it will load all the slow stuff before the app is shown
             return
 
-        self.mode = None
+        self._mode = None
         self.query = Query(None, query_str)
 
         for mode in get_modes():
             if query := mode.parse_query_str(query_str):
-                self.mode = mode
+                self._mode = mode
                 self.query = query
                 break
 
@@ -75,24 +75,24 @@ class UlauncherCore:
         query_str = self.query.argument
         if not query_str:
             return []
-        sorted_ = sorted(self.triggers, key=lambda i: i.search_score(query_str), reverse=True)[:limit]
+        sorted_ = sorted(self._triggers, key=lambda i: i.search_score(query_str), reverse=True)[:limit]
         return list(filter(lambda searchable: searchable.search_score(query_str) > min_score, sorted_))
 
     def get_initial_results(self, limit: int) -> Iterable[Result]:
         """Called if the query is empty (on startup or when you delete the query)"""
         app_mode = get_app_mode()
         for app_result in app_mode.get_initial_results(limit):
-            self.mode_map[app_result] = app_mode
+            self._mode_map[app_result] = app_mode
             yield app_result
 
     def handle_change(self) -> None:
         from ulauncher.modes.mode_handler import handle_action
 
-        if self.mode:
+        if self._mode:
             try:
-                handle_action(self.mode.handle_query(self.query))
+                handle_action(self._mode.handle_query(self.query))
             except Exception:
-                logger.exception("Mode '%s' triggered an error while handling query '%s'", self.mode, self.query)
+                logger.exception("Mode '%s' triggered an error while handling query '%s'", self._mode, self.query)
             return
 
         # No mode selected, which means search
@@ -102,12 +102,12 @@ class UlauncherCore:
             for mode in get_modes():
                 for fallback_result in mode.get_fallback_results(str(self.query)):
                     results.append(fallback_result)
-                    self.mode_map[fallback_result] = mode
+                    self._mode_map[fallback_result] = mode
         handle_action(results)
 
     def handle_backspace(self, query_str: str) -> bool:
-        if self.mode:
-            new_query = self.mode.handle_backspace(query_str)
+        if self._mode:
+            new_query = self._mode.handle_backspace(query_str)
             if new_query is not None:
                 self.query = new_query
                 _events.emit("app:set_query", str(new_query))
@@ -115,7 +115,7 @@ class UlauncherCore:
         return False
 
     def activate_result(self, result: Result, alt: bool = False) -> None:
-        mode = self.mode_map.get(result, self.mode)
+        mode = self._mode_map.get(result, self._mode)
         if not mode:
             logger.warning("Cannot activate result '%s' because no mode is set", result)
             return
