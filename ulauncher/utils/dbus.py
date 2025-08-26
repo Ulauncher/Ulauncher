182360982b1d64f8abe8c928e0199778d484fc0c
Albin Larsson <515120+friday@users.noreply.github.com>
2025-08-27 01:19:19 +0200

    refactor(dbus): clarify which dbus methods are general vs ulauncher app specific
    The new is_app_running method intentionally takes an argument so we can potentially use
    it to check other apps or other versions of Ulauncher (we could use this
    to improve kill_ulauncher_v5() or potentially the launch_app command
    (probably not though).
    
    The older ones are specifically for communicating with the Ulauncher app
    under the current app id.
    
    Also removed exception handling because we should always have Gio and if
    it raises an error we don't want to default to anything.

--- ulauncher/utils/dbus.py
+++ ulauncher/utils/dbus.py
@@ -12,38 +12,34 @@ from ulauncher import app_id, dbus_path
 logger = logging.getLogger(__name__)
 
 
+def is_app_running(app_id: str) -> bool:
+    """Check if app is running by checking if the D-Bus service name is owned."""
+    bus = Gio.bus_get_sync(Gio.BusType.SESSION)
+    result = bus.call_sync(
+        "org.freedesktop.DBus",
+        "/org/freedesktop/DBus",
+        "org.freedesktop.DBus",
+        "NameHasOwner",
+        GLib.Variant("(s)", (app_id,)),
+        GLib.VariantType("(b)"),
+        Gio.DBusCallFlags.NONE,
+        -1,
+    )
+    unpacked = result.unpack()
+    return bool(unpacked[0])
+
+
 @lru_cache(maxsize=1)
-def get_dbus_action_group() -> Gio.DBusActionGroup:
+def get_ulauncher_dbus_action_group() -> Gio.DBusActionGroup:
     bus = Gio.bus_get_sync(Gio.BusType.SESSION)
     return Gio.DBusActionGroup.get(bus, app_id, dbus_path)
 
 
-def is_app_running(app_id: str) -> bool:
-    """Check if Ulauncher app is running by checking if the D-Bus service name is owned."""
-    try:
-        bus = Gio.bus_get_sync(Gio.BusType.SESSION)
-        result = bus.call_sync(
-            "org.freedesktop.DBus",
-            "/org/freedesktop/DBus",
-            "org.freedesktop.DBus",
-            "NameHasOwner",
-            GLib.Variant("(s)", (app_id,)),
-            GLib.VariantType("(b)"),
-            Gio.DBusCallFlags.NONE,
-            -1,
-        )
-        unpacked = result.unpack()
-        return bool(unpacked[0])
-    except Exception as e:  # noqa: BLE001
-        logger.debug("Failed to check if Ulauncher is running: %s", e)
-        return False
-
-
 def dbus_trigger_event(name: str, message: Any | None = None) -> None:
-    """Sends a D-Bus message to the App, which is delegated to the EventBus listener matching the name."""
+    """Sends a D-Bus message to the Ulauncher App, which is delegated to the EventBus listener matching the name."""
     if not is_app_running(app_id):
         logger.debug("Ulauncher app is not running, skipping D-Bus trigger event: %s", name)
         return
 
     json_message = json.dumps({"name": name, "message": message})
-    get_dbus_action_group().activate_action("trigger-event", GLib.Variant.new_string(json_message))
+    get_ulauncher_dbus_action_group().activate_action("trigger-event", GLib.Variant.new_string(json_message))
