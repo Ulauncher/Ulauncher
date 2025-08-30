bf3d6b768ae0614346f7ab501ae2aa208cd79e8b
Albin Larsson <515120+friday@users.noreply.github.com>
2025-08-30 20:07:55 +0200

    fix: support getting ulauncher v5 pid without needing legacy dbus dependency
    since ulauncher v5 will be uninstalled but running when this runs, the
    dbus dependency might be also, so this uses the gio dbus lib instead to
    do the same thing (more convoluted though)

--- ulauncher/utils/dbus.py
+++ ulauncher/utils/dbus.py
@@ -12,34 +12,67 @@ from ulauncher import app_id, dbus_path
 logger = logging.getLogger(__name__)
 
 
-def is_app_running(app_id: str) -> bool:
-    """Check if app is running by checking if the D-Bus service name is owned."""
-    bus = Gio.bus_get_sync(Gio.BusType.SESSION)
-    result = bus.call_sync(
-        "org.freedesktop.DBus",
-        "/org/freedesktop/DBus",
-        "org.freedesktop.DBus",
-        "NameHasOwner",
-        GLib.Variant("(s)", (app_id,)),
-        GLib.VariantType("(b)"),
-        Gio.DBusCallFlags.NONE,
-        -1,
+def dbus_query_freedesktop(
+    bus: Gio.DBusConnection,
+    method_name: str,
+    parameters: GLib.Variant | None = None,
+    response_type: GLib.VariantType | None = None,
+    flags: Gio.DBusCallFlags | None = None,
+    timeout_msec: int | None = None,
+    cancellable: Gio.Cancellable | None = None,
+) -> GLib.Variant:
+    """Helper function to query the standard D-Bus interface org.freedesktop.DBus."""
+    return bus.call_sync(
+        bus_name="org.freedesktop.DBus",
+        object_path="/org/freedesktop/DBus",
+        interface_name="org.freedesktop.DBus",
+        method_name=method_name,
+        parameters=parameters,
+        reply_type=response_type,
+        flags=flags or Gio.DBusCallFlags.NONE,
+        timeout_msec=timeout_msec or -1,
+        cancellable=cancellable,
     )
-    unpacked = result.unpack()
-    return bool(unpacked[0])
+
+
+def check_app_running(app_id: str, bus: Gio.DBusConnection | None = None) -> bool:
+    """Check if app is running by checking if the D-Bus service name is owned."""
+    params = GLib.Variant("(s)", (app_id,))
+    response_type = GLib.VariantType("(b)")
+    bus = bus or Gio.bus_get_sync(Gio.BusType.SESSION)
+    (is_running,) = dbus_query_freedesktop(bus, "NameHasOwner", params, response_type).unpack()
+    return bool(is_running)
+
+
+def get_app_pid(app_id: str) -> int | None:
+    """Get the PID of a D-Bus app"""
+    bus = Gio.bus_get_sync(Gio.BusType.SESSION)
+
+    if not check_app_running(app_id, bus):
+        return None
+
+    get_owner_params = GLib.Variant("(s)", (app_id,))
+    get_owner_response_type = GLib.VariantType("(s)")
+    (owner,) = dbus_query_freedesktop(bus, "GetNameOwner", get_owner_params, get_owner_response_type).unpack()
+
+    get_pid_params = GLib.Variant("(s)", (owner,))
+    get_pid_response_type = GLib.VariantType("(u)")
+    (pid,) = dbus_query_freedesktop(bus, "GetConnectionUnixProcessID", get_pid_params, get_pid_response_type).unpack()
+    return int(pid)
 
 
 @lru_cache(maxsize=1)
-def get_ulauncher_dbus_action_group() -> Gio.DBusActionGroup:
-    bus = Gio.bus_get_sync(Gio.BusType.SESSION)
+def get_ulauncher_dbus_action_group(bus: Gio.DBusConnection) -> Gio.DBusActionGroup:
     return Gio.DBusActionGroup.get(bus, app_id, dbus_path)
 
 
 def dbus_trigger_event(name: str, message: Any | None = None) -> None:
     """Sends a D-Bus message to the Ulauncher App, which is delegated to the EventBus listener matching the name."""
-    if not is_app_running(app_id):
+    bus = Gio.bus_get_sync(Gio.BusType.SESSION)
+
+    if not check_app_running(app_id, bus):
         logger.debug("Ulauncher app is not running, skipping D-Bus trigger event: %s", name)
         return
 
     json_message = json.dumps({"name": name, "message": message})
-    get_ulauncher_dbus_action_group().activate_action("trigger-event", GLib.Variant.new_string(json_message))
+    get_ulauncher_dbus_action_group(bus).activate_action("trigger-event", GLib.Variant.new_string(json_message))
--- ulauncher/utils/v5_killer.py
+++ ulauncher/utils/v5_killer.py
@@ -19,34 +19,20 @@ def kill_ulauncher_v5() -> None:
     if not first_v6_run:
         return
 
-    try:
-        import dbus
-    except ImportError:
-        logger.info("DBus is not available. Cannot check for Ulauncher v5 instance.")
+    # Find the Ulauncher v5 service on the session bus
+    from ulauncher.utils.dbus import get_app_pid
+
+    pid = get_app_pid(v5_service_name)
+
+    if not pid:
         return
 
-    bus = dbus.SessionBus()
+    logger.info("Ulauncher v5 is running with PID: %s. Killing...", pid)
+
     try:
-        # Find the Ulauncher v5 service on the session bus
-        dbus_proxy = bus.get_object("org.freedesktop.DBus", "/org/freedesktop/DBus")
-        dbus_iface = dbus.Interface(dbus_proxy, dbus_interface="org.freedesktop.DBus")
-        names = dbus_iface.ListNames()
-        if v5_service_name not in names:
-            return
-
-        # Ask for PIDs owning this connection
-        owner = dbus_iface.GetNameOwner(v5_service_name)
-        pid = dbus_iface.GetConnectionUnixProcessID(owner)
-        logger.info("Ulauncher v5 is running with PID: %s. Killing...", pid)
-
-        try:
-            os.kill(pid, signal.SIGTERM)
-            logger.info("PID: %s killed.", pid)
-        except ProcessLookupError:
-            logger.info("Process with PID %s not found (already dead?).", pid)
-        except Exception as ex:  # noqa: BLE001
-            logger.info("Failed to kill process: %s", ex)
-            return
-    except dbus.DBusException as ex:
-        logger.exception("Failed to communicate with DBus: %s", ex.get_dbus_message())
-        return
+        os.kill(pid, signal.SIGTERM)
+        logger.info("PID: %s killed.", pid)
+    except ProcessLookupError:
+        logger.info("Process with PID %s not found (already dead?).", pid)
+    except Exception as ex:  # noqa: BLE001
+        logger.info("Failed to kill process: %s", ex)
