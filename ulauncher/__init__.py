58b42f63d23b0f2bf627d837bb495227fdbac05b
Albin Larsson <515120+friday@users.noreply.github.com>
2025-09-01 00:13:55 +0200

    chore(release): v6.0.0-beta24

--- ulauncher/__init__.py
+++ ulauncher/__init__.py
@@ -6,7 +6,7 @@ from . import paths
 
 app_id = "io.ulauncher.Ulauncher"
 dbus_path = "/" + app_id.replace(".", "/")
-version = "6.0.0-beta23"
+version = "6.0.0-beta24"
 api_version = "3.0"
 first_run = not os.path.exists(paths.CONFIG)  # If there is no config dir, assume it's the first run
 first_v6_run = not os.path.exists(paths.STATE)
