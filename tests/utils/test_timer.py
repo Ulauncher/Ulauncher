64c39bbc237d9fce71c6900a894c0dda5982c7dc
Albin Larsson <515120+friday@users.noreply.github.com>
2025-12-06 19:53:55 +0100

    refactor: simplify glib timer wrapper

--- tests/utils/test_timer.py
+++ tests/utils/test_timer.py
@@ -12,22 +12,11 @@ class TestTimer:
     def glib(self, mocker: MockerFixture) -> MagicMock:
         return mocker.patch("ulauncher.utils.timer.GLib")
 
-    def test_timer_subsecond(self, glib: MagicMock) -> None:
+    def test_timer(self, glib: MagicMock) -> None:
         func = Mock()
-        subsecond_time = 0.5
-        ctx = timer(subsecond_time, func)
-        glib.timeout_source_new.assert_called_with(subsecond_time * 1000)
-        src: Any = ctx.source
-        ctx.trigger(None)
-        func.assert_called_once()
-        ctx.cancel()
-        src.destroy.assert_called_once()
-
-    def test_timer_second(self, glib: MagicMock) -> None:
-        func = Mock()
-        seconds_time = 2
-        ctx = timer(seconds_time, func)
-        glib.timeout_source_new_seconds.assert_called_with(seconds_time)
+        time = 0.1
+        ctx = timer(time, func)
+        glib.timeout_source_new.assert_called_with(time * 1000)
         src: Any = ctx.source
         ctx.trigger(None)
         func.assert_called_once()
