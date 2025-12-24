cdcf8aca5ff1b6f0c177a426393c43aec2998ff9
Albin Larsson <515120+friday@users.noreply.github.com>
2025-12-24 16:13:27 +0100

    fix: fix validation messages misleading if passing falsy value that's not a str

--- ulauncher/internals/actions.py
+++ ulauncher/internals/actions.py
@@ -87,26 +87,26 @@ def copy(text: str) -> Copy:
 
 
 def open(item: str) -> Open:  # noqa: A001
-    if not item:
-        msg = "Open argument cannot be empty"
-        logger.error(msg)
-        raise ValueError(msg)
     if not isinstance(item, str):
         msg = f'Open argument "{item}" is invalid. It must be a string'
         logger.error(msg)
         raise TypeError(msg)
+    if not item:
+        msg = "Open argument cannot be empty"
+        logger.error(msg)
+        raise ValueError(msg)
     return {"type": "action:open", "data": item}
 
 
 def run_script(script: str, args: str = "") -> LegacyRunScript:
-    if not script:
-        msg = "Script argument cannot be empty"
-        logger.error(msg)
-        raise ValueError(msg)
     if not isinstance(script, str):
         msg = f'Script argument "{script}" is invalid. It must be a string'
         logger.error(msg)
         raise TypeError(msg)
+    if not script:
+        msg = "Script argument cannot be empty"
+        logger.error(msg)
+        raise ValueError(msg)
     return {"type": "action:legacy_run_script", "data": [script, args]}
 
 
