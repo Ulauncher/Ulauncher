e55c01bbbda6c0ebddec8335c92add2f428806c0
Albin Larsson <515120+friday@users.noreply.github.com>
2026-01-04 21:33:08 +0100

    refactor: avoid duplicate import

--- ulauncher/modes/shortcuts/run_shortcut.py
+++ ulauncher/modes/shortcuts/run_shortcut.py
@@ -3,11 +3,10 @@ from __future__ import annotations
 import re
 
 from ulauncher.internals import actions
-from ulauncher.internals.actions import ActionMessage
 from ulauncher.modes.shortcuts.run_script import run_script
 
 
-def run_shortcut(command: str, arg: str | None = None) -> ActionMessage:
+def run_shortcut(command: str, arg: str | None = None) -> actions.ActionMessage:
     command = command.strip()
     if arg:
         command = command.replace("%s", arg)
