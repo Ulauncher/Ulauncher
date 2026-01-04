f09381cdc917bceb92c8f536557ca21bff88d04e
Albin Larsson <515120+friday@users.noreply.github.com>
2026-01-04 14:11:25 +0100

    refactor: add types to legacy event listener

--- ulauncher/api/client/EventListener.py
+++ ulauncher/api/client/EventListener.py
@@ -1,9 +1,11 @@
 from __future__ import annotations  # noqa: N999
 
-from typing import Any
+from typing import Iterable
 
 import ulauncher.api.extension
 import ulauncher.api.shared.event
+from ulauncher.internals import actions
+from ulauncher.internals.result import Result
 
 
 class EventListener:
@@ -12,8 +14,9 @@ class EventListener:
     """
 
     def on_event(
-        self, event: ulauncher.api.shared.event.BaseEvent, extension: ulauncher.api.extension.Extension
-    ) -> Any:
+        self, _event: ulauncher.api.shared.event.BaseEvent, _extension: ulauncher.api.extension.Extension
+    ) -> actions.ActionMessage | Iterable[Result]:
         """
-        :return: Action to run
+        :return: Action to run (for KeywordQueryEvent, this should only return Iterable[Result])
         """
+        return actions.do_nothing()
