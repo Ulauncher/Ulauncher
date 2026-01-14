9a37492927ac80fe46f6e6140472c283a1e9c92a
Albin Larsson <515120+friday@users.noreply.github.com>
2026-01-14 20:29:38 +0100

    refactor: improve layer shell namespace comment

--- ulauncher/ui/layer_shell.py
+++ ulauncher/ui/layer_shell.py
@@ -32,7 +32,7 @@ def enable(window: Gtk.Window) -> bool:
         return False
 
     GtkLayerShell.init_for_window(window)
-    # Set namespace otherwise it defaults to `gtk-layer-shell`
+    # Set namespace (allowing users of hyprland and possibly other WM/DEs to add config exceptions for Ulauncher)
     GtkLayerShell.set_namespace(window, "ulauncher")
     GtkLayerShell.set_keyboard_mode(window, GtkLayerShell.KeyboardMode.EXCLUSIVE)
     GtkLayerShell.set_layer(window, GtkLayerShell.Layer.OVERLAY)
