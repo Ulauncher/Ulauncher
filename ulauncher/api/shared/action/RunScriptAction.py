3301f35db610eb1896559822b70ff63693df57d6
Albin Larsson <515120+friday@users.noreply.github.com>
2026-01-14 18:29:32 +0100

    fix: RunScriptAction runs too early
    restores old RunScriptAction logic, undoing 1dbb9882adac7cdbe016f5348ddf59e830b7c21b
    
    fixes: #1633

--- ulauncher/api/shared/action/RunScriptAction.py
+++ ulauncher/api/shared/action/RunScriptAction.py
@@ -1,19 +1,11 @@
-from __future__ import annotations  # noqa: N999
-
-import logging
-
-from ulauncher.internals import effects
-from ulauncher.modes.shortcuts.run_script import run_script
-
-logger = logging.getLogger()
+from ulauncher.internals import effects  # noqa: N999
 
 
-def RunScriptAction(script: str, arg: str = "") -> effects.CloseWindow:  # noqa: N802
+def RunScriptAction(script: str, arg: str = "") -> effects.LegacyRunScript:  # noqa: N802
     if not isinstance(script, str):
         msg = f'Script argument "{script}" is invalid. It must be a string'
         raise TypeError(msg)
     if not script:
         msg = "Script argument cannot be empty"
         raise ValueError(msg)
-    run_script(script, arg)
-    return effects.close_window()
+    return {"type": effects.EffectType.LEGACY_RUN_SCRIPT, "data": [script, arg]}
