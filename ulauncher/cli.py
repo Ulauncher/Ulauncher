7af089ce5693cac265a16072f0ae2020258c0d26
Albin Larsson <515120+friday@users.noreply.github.com>
2026-04-06 16:36:33 +0200

    refactor: remove unused localization plumbing (#1669)
    Ulauncher has never had localization data. I think these were just remnants of the original scaffolding or copy/pasting code.
    
    Even if we did have localization data, the text domain must be set from gettext too as I understand it.
    
    When/if we decide to support localization we might not even use these specific implementation details anyway. They are just misleading.

--- ulauncher/cli.py
+++ ulauncher/cli.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import argparse
 from functools import lru_cache, partial
-from gettext import gettext as _
 
 from ulauncher import version
 from ulauncher.modes.extensions.extension_cli_handlers import (
@@ -35,18 +34,18 @@ def get_cli_args() -> CLIArguments:
         "Ulauncher is a GTK application launcher with support for extensions, shortcuts (scripts), calculator, file browser and custom themes.",  # noqa: E501
         add_help=False,
     )
-    parser.add_argument("-h", "--help", action="help", help=_("Show this help message and exit"))
-    parser.add_argument("-v", "--verbose", action="store_true", help=_("Show debug log messages"))
+    parser.add_argument("-h", "--help", action="help", help="Show this help message and exit")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Show debug log messages")
     parser.add_argument(
         "--version",
         action="version",
-        help=_("Show version number and exit"),
+        help="Show version number and exit",
         version=f"Ulauncher {version}",
     )
     parser.add_argument(
         "--daemon",
         action="store_true",
-        help=_("Run Ulauncher as a background process, without initially showing the window"),
+        help="Run Ulauncher as a background process, without initially showing the window",
     )
     # deprecated
     parser.add_argument("--dev", action="store_true", help=argparse.SUPPRESS)
