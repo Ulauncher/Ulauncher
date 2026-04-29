76231b99707a811efcaab529dd94d89939f62808
Albin Larsson <515120+friday@users.noreply.github.com>
2026-04-29 20:00:32 +0200

    refactor: fix types for other decorators

--- ulauncher/utils/decorator/run_async.py
+++ ulauncher/utils/decorator/run_async.py
@@ -1,9 +1,16 @@
+from __future__ import annotations
+
 from functools import wraps
 from threading import Thread
-from typing import Any, Callable
+from typing import TYPE_CHECKING, Any, Callable, cast
+
+if TYPE_CHECKING:
+    from typing_extensions import ParamSpec
+
+    P = ParamSpec("P")
 
 
-def run_async(func: Callable[..., Any]) -> Callable[..., Thread]:
+def run_async(func: Callable[P, Any]) -> Callable[P, Thread]:
     """
     Function decorator, intended to make a callable run in a separate thread (asynchronously).
     We still need this because asyncio is absolutely garbage when you want to know
@@ -26,4 +33,4 @@ def run_async(func: Callable[..., Any]) -> Callable[..., Thread]:
         thread.start()
         return thread
 
-    return async_func
+    return cast("Callable[P, Thread]", async_func)
