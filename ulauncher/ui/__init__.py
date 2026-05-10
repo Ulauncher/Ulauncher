96d9807210378ce3d48f509be29d37a9e052cfe2
Albin Larsson <515120+friday@users.noreply.github.com>
2026-05-10 22:34:03 +0200

    refactor: add comment for verision pinning
    Also change formatting to one per line

--- ulauncher/ui/__init__.py
+++ ulauncher/ui/__init__.py
@@ -1,3 +1,12 @@
 import gi
 
-gi.require_versions({"Gdk": "3.0", "GdkPixbuf": "2.0", "GdkX11": "3.0", "Gtk": "3.0", "Pango": "1.0"})
+# pin gi versions for all use inside the ui directory tree
+gi.require_versions(
+    {
+        "Gdk": "3.0",
+        "GdkPixbuf": "2.0",
+        "GdkX11": "3.0",
+        "Gtk": "3.0",
+        "Pango": "1.0",
+    }
+)
