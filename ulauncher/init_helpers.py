358287138a4723fe89fe856d6c6faee29919d352
Albin Larsson <515120+friday@users.noreply.github.com>
2026-05-10 23:29:07 +0200

    fix: handle potential libX11.so import OSError

--- ulauncher/init_helpers.py
+++ ulauncher/init_helpers.py
@@ -60,7 +60,11 @@ def init_x11_threads() -> None:
     # discussion of this ABI version back to before 2009, 12 years prior to when this code was added.
     # Also, with most of the development focus on Wayland, the chance of a ABI version bump in X11 is
     # exceptionally small. Therefore, no additional fancy discovery methods are necessary.
-    x11 = cdll.LoadLibrary("libX11.so.6")
+    try:
+        x11 = cdll.LoadLibrary("libX11.so.6")
+    except OSError as e:
+        err_msg = "Failed to load libX11.so.6. Ensure X11 libraries are installed on your system."
+        raise OSError(err_msg) from e
 
     # In order to use any X11 calls from multiple threads, the X11 library must be initialized to use
     # mutex protection. XInitThreads() sets this up and must be called before any other X11
