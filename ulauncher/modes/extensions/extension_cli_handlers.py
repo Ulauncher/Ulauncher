4a2bf8addabb7abd1cc459cc54785d85b6a0348b
Albin Larsson <515120+friday@users.noreply.github.com>
2026-05-11 22:58:25 +0200

    fix(cli): ext update warning should use normalized id in warning

--- ulauncher/modes/extensions/extension_cli_handlers.py
+++ ulauncher/modes/extensions/extension_cli_handlers.py
@@ -115,7 +115,7 @@ def upgrade_extensions(_: ArgumentParser, args: Namespace) -> bool:
             try:
                 asyncio.run(controller.update())
             except ext_exceptions.UrlError:
-                _warn_url_error(args.input, controller.state.url)
+                _warn_url_error(controller.id, controller.state.url)
                 return False
             except ext_exceptions.RemoteError:
                 logger.warning("Network error: Could not upgrade %s", args.input)
