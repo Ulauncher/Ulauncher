dd66db3a5e0c480519ae35ae1fe2b2eb36897140
Albin Larsson <515120+friday@users.noreply.github.com>
2026-05-15 14:36:49 +0200

    refactor: avoid declariing legacy args table in function call

--- ulauncher/cli/__init__.py
+++ ulauncher/cli/__init__.py
@@ -10,6 +10,15 @@ from ulauncher.data import BaseDataClass
 from ulauncher.init_helpers import configure_logging, ensure_runtime_dirs
 from ulauncher.utils.lru_cache import lru_cache
 
+_LEGACY_ARGS: dict[str, tuple[str | None, str]] = {
+    "dev": ("verbose", "use --verbose"),
+    "no_window": ("daemon", "use --daemon"),
+    # intentionally break hide_window to prevent legacy XDG autostart entries from starting
+    "hide_window": (None, "use --daemon"),
+    "no_extensions": (None, "see --help for available commands"),
+    "no_window_shadow": (None, "configure via the Window shadow size setting in preferences"),
+}
+
 CommandName = Literal["extensions", "install", "uninstall", "upgrade", "preview"]
 
 
@@ -175,16 +184,7 @@ def parse(input_args: list[str]) -> CLIArguments:
     args = parser.parse_args(args=input_args)
     namespace = vars(args)
 
-    legacy_args: dict[str, tuple[str | None, str]] = {
-        "dev": ("verbose", "use --verbose"),
-        "no_window": ("daemon", "use --daemon"),
-        # intentionally break hide_window to prevent legacy XDG autostart entries from starting
-        "hide_window": (None, "use --daemon"),
-        "no_extensions": (None, "see --help for available commands"),
-        "no_window_shadow": (None, "configure via the Window shadow size setting in preferences"),
-    }
-
-    for legacy_arg, (shim, hint) in legacy_args.items():
+    for legacy_arg, (shim, hint) in _LEGACY_ARGS.items():
         if namespace.pop(legacy_arg, False):
             msg = f"The --{legacy_arg.replace('_', '-')} argument has been removed ({hint})"
             if sys.stderr.isatty():
