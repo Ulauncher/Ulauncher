e0bac2ced8fc9856453252d9e008ad25f96e7352
Albin Larsson <515120+friday@users.noreply.github.com>
2026-08-28 08:43:03 +0200

    fix(app): position the window before mapping it, not after
    Position the window before presenting it, so it always maps with
    coordinates computed for the monitor the user is on.
    
    Only affects X11.
    
    Hopefully fixes #563, #661

--- ulauncher/ui/windows/UlauncherWindow.py
+++ ulauncher/ui/windows/UlauncherWindow.py
@@ -284,10 +284,9 @@ class UlauncherWindow(Gtk.Window, WindowHelper):
                   screen['height'] / 5 + screen['y'])
 
     def show_window(self):
-        # works only when the following methods are called in that exact order
+        self.position_window()
         self.window.set_sensitive(True)
         self.window.present()
-        self.position_window()
         if not is_wayland_compatibility_on():
             self.present_with_time(Keybinder.get_current_event_time())
 
