2f9ff08149d299b199336f69d39a1396bfc1f907
Albin Larsson <515120+friday@users.noreply.github.com>
2026-08-31 19:55:24 +0200

    test: register with_display pytest marker

--- conftest.py
+++ conftest.py
@@ -9,6 +9,10 @@ import pytest
 is_display_enabled = bool(GdkX11.X11Display.get_default())
 
 
+def pytest_configure(config):
+    config.addinivalue_line("markers", "with_display: skip test if no display is available")
+
+
 def pytest_runtest_setup(item):
     if isinstance(item, pytest.Function):
         if any(item.iter_markers('with_display')) and not is_display_enabled:
